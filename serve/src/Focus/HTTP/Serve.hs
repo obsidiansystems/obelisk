cc77d3c492a6fda370fe61675987978401909658
hsloan <ishaqsloan@gmail.com>
2017-08-22 15:03:52 -0400

    initial commit

---
+++ serve/src/Focus/HTTP/Serve.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE CPP, OverloadedStrings, TemplateHaskell #-}
+module Focus.HTTP.Serve where
+
+import Focus.HTTP.Accept
+
+import Snap
+import Snap.Util.FileServe
+
+import Control.Applicative
+import Control.Exception (try, throwIO, catchJust)
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Attoparsec.ByteString (parseOnly, endOfInput)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.List
+import Data.Monoid
+import qualified Data.Text as T
+import Data.Text.Encoding
+import System.Directory
+import System.FilePath
+import System.IO.Error
+import System.Posix (getFileStatus, fileSize)
+
+cachePermanently :: MonadSnap m => m ()
+cachePermanently = do
+  modifyResponse $ setHeader "Cache-Control" "public, max-age=315360000"
+  modifyResponse $ setHeader "Expires" "Tue, 01 Feb 2050 00:00:00 GMT" --TODO: This should be set to "approximately one year from the time the response is sent"
+
+doNotCache :: MonadSnap m => m ()
+doNotCache = do
+  modifyResponse $ setHeader "Cache-Control" "no-cache, no-store, must-revalidate" 
+  modifyResponse $ setHeader "Expires" "0"
+
+serveAssets :: MonadSnap m => FilePath -> FilePath -> m ()
+serveAssets = serveAssets' True
+
+serveAssetsInPlace :: MonadSnap m => FilePath -> FilePath -> m ()
+serveAssetsInPlace = serveAssets' False
+
+serveAssets' :: MonadSnap m => Bool -> FilePath -> FilePath -> m ()
+serveAssets' doRedirect base fallback = do
+  pRaw <- getSafePath
+  let go p = do
+        assetType <- liftIO $ try $ BS.readFile $ base </> p </> "type"
+        case assetType of
+          Right "immutable" -> do
+            conditionalOnModification <- getsRequest $ getHeader "If-Modified-Since"
+            case conditionalOnModification of
+              Nothing -> do
+                encodedFiles <- liftM (filter (`notElem` [".", ".."])) $ liftIO $ getDirectoryContents $ base </> p </> "encodings"
+                availableEncodings <- liftM (map snd . sort) $ forM encodedFiles $ \f -> do
+                  stat <- liftIO $ getFileStatus $ base </> p </> "encodings" </> f
+                  return (fileSize stat, Encoding $ encodeUtf8 $ T.pack f)
+                acceptEncodingRaw <- getsRequest $ getHeader "Accept-Encoding"
+                ae <- case acceptEncodingRaw of
+                  Nothing -> return missingAcceptableEncodings
+                  Just aer -> case parseOnly (acceptEncodingBody <* endOfInput) aer of
+                    Right ae -> return ae
+                    Left err -> error err
+                Just (Encoding e) <- return $ chooseEncoding availableEncodings ae
+                modifyResponse $ setHeader "Content-Encoding" e . setHeader "Vary" "Accept-Encoding"
+                if doRedirect then cachePermanently else doNotCache --TODO: Use Etags when not redirecting
+                let finalFilename = base </> p </> "encodings" </> T.unpack (decodeUtf8 e)
+                stat <- liftIO $ getFileStatus finalFilename
+                modifyResponse $ setHeader "Last-Modified" "Thu, 1 Jan 1970 00:00:00 GMT"
+                modifyResponse $ setResponseCode 200 . setContentLength (fromIntegral $ fileSize stat) . setContentType (fileType defaultMimeTypes p)
+                sendFile finalFilename
+              Just _ -> cachePermanently >> modifyResponse (setResponseCode 304)
+          Right "redirect" -> do
+            mtarget <- liftIO $ getAssetTarget $ base </> p
+            case mtarget of
+              Just target -> if doRedirect
+                             then do
+                               doNotCache
+                               redirect target
+                             else go $ takeDirectory p </> T.unpack (decodeUtf8 target)
+              Nothing -> serveFile $ fallback </> p
+          Right unknown -> error $ T.unpack ("serveAssets': Unknown asset " <> decodeUtf8 unknown)
+          Left err | isDoesNotExistError err -> (doNotCache >> serveFileIfExists (fallback </> p)) <|> do
+                       let (dirname, filename) = splitFileName p
+                           unhashedFilename = drop 1 $ dropWhile (/= '-') filename
+                       if null unhashedFilename then pass else do
+                         doNotCache
+                         serveFileIfExists $ fallback </> dirname </> unhashedFilename
+                   | otherwise -> liftIO $ throwIO err
+  go $ if "/" `isSuffixOf` pRaw || pRaw == "" then pRaw <> "index.html" else pRaw
+
+serveFileIfExists :: MonadSnap m => FilePath -> m ()
+serveFileIfExists f = do
+  exists <- liftIO $ doesFileExist f
+  if exists then serveFile f else pass
+
+serveFileIfExistsAs :: MonadSnap m => ByteString -> FilePath -> m ()
+serveFileIfExistsAs mimeType f = do
+  exists <- liftIO $ doesFileExist f
+  if exists then serveFileAs mimeType f else pass
+
+getAssetTarget :: FilePath -> IO (Maybe ByteString)
+getAssetTarget p = catchJust (\e -> if isDoesNotExistErrorType (ioeGetErrorType e) then Just () else Nothing) (liftM Just $ BS.readFile $ p </> "target") (\_ -> return Nothing)
+
+getAssetPath :: FilePath -> FilePath -> IO (Maybe FilePath)
+getAssetPath base p = do
+  target <- getAssetTarget $ base </> p
+  return $ fmap ((takeDirectory p </>) . T.unpack . decodeUtf8) target
---
+++ th/src/Focus/HTTP/TH.hs
@@ -0,0 +1,46 @@
+module Focus.HTTP.TH where
+
+import Language.Haskell.TH.Syntax
+import System.FilePath
+import qualified Data.Text as T
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Builder as LBS
+import Data.Digest.Pure.SHA
+import Data.Text.Encoding
+import Data.Monoid
+import Data.Bits
+import qualified Data.Vector.Unboxed as UV
+import Data.Char
+import Data.Word
+
+-- | Warning: does not support negative numbers (will result in infinite loop)
+toNixBase32 :: LBS.ByteString -> LBS.ByteString
+toNixBase32 x = LBS.toLazyByteString $ mconcat $ map (LBS.word8 . (symbols UV.!) . fromIntegral) vals
+  where vals = byteStringToQuintets x
+        symbols = UV.fromList $ map (fromIntegral . ord) $ filter (`notElem` "eotu") $ ['0'..'9'] <> ['a'..'z']
+
+
+-- See https://github.com/NixOS/nix/blob/6f1743b1a5116ca57a60b481ee4083c891b7a334/src/libutil/hash.cc#L109
+byteStringToQuintets :: LBS.ByteString -> [Word8]
+byteStringToQuintets hash = map f [len-1, len-2 .. 0]
+  where hashSize = fromIntegral $ LBS.length hash
+        len = (hashSize * 8 - 1) `div` 5 + 1
+        f n = let b = n * 5
+                  (i, j) = b `divMod` 8
+                  j' = fromIntegral j
+                  c = ((hash `LBS.index` i) `shift` (-j')) .|. (if i >= hashSize - 1 then 0 else (hash `LBS.index` (i + 1)) `shift` (8 - j')) --TODO: This is probably pretty slow; replace with something that doesn't use LBS.index
+              in c .&. 0x1f
+
+assetPath' :: FilePath -> FilePath -> Q FilePath
+assetPath' root relativePath = do
+  let path = root </> relativePath
+  qAddDependentFile path
+  contents <- fmap LBS.fromStrict $ runIO $ BS.readFile path
+  let hashPrefix = T.unpack $ decodeUtf8 $ LBS.toStrict $ toNixBase32 $ bytestringDigest $ sha256 contents
+      (dir, filename) = splitFileName relativePath
+  return $ dir </> (hashPrefix <> "-" <> filename)
+
+assetPath :: FilePath -> FilePath -> Q Exp
+assetPath root relativePath =
+  LitE . StringL <$> assetPath' root relativePath
