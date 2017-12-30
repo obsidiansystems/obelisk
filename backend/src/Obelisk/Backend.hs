3db6a57b86b667e3883c2edf0ec4b8886efc7bae
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-12-30 15:32:11 -0500

    Serve static assets and frontend.jsexe separately

--- backend/src/Obelisk/Backend.hs
+++ backend/src/Obelisk/Backend.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Obelisk.Backend
   ( backend
   , BackendConfig
@@ -9,7 +10,7 @@ import qualified Data.ByteString.Char8 as BSC8
 import Data.Default (Default (..))
 import Obelisk.Asset.Serve (serveAssets)
 import System.IO (hSetBuffering, stderr, BufferMode (..))
-import Snap (httpServe, defaultConfig, commandLineConfig)
+import Snap (httpServe, defaultConfig, commandLineConfig, route)
 import Snap.Internal.Http.Server.Config (Config (accessLog, errorLog), ConfigLog (ConfigIoLog))
 
 --TODO: Add a link to a large explanation of the idea of using 'def'
@@ -34,4 +35,7 @@ backend _ = do
         , errorLog = Just $ ConfigIoLog BSC8.putStrLn
         }
   -- Start the web server
-  httpServe httpConf $ serveAssets "static.assets" "static"
+  httpServe httpConf $ route
+    [ ("", serveAssets "frontend.jsexe.assets" "frontend.jsexe")
+    , ("", serveAssets "static.assets" "static")
+    ]
