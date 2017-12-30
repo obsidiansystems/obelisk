cbc197daefd0344d5994ca6d1e2bafe3c28e15da
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-12-29 21:16:21 -0500

    Lots of changes and reorganization

---
+++ backend/src/Obelisk/Backend.hs
@@ -0,0 +1,37 @@
+module Obelisk.Backend
+  ( backend
+  , BackendConfig
+  -- * Re-exports
+  , Default (def)
+  ) where
+
+import qualified Data.ByteString.Char8 as BSC8
+import Data.Default (Default (..))
+import Obelisk.Asset.Serve (serveAssets)
+import System.IO (hSetBuffering, stderr, BufferMode (..))
+import Snap (httpServe, defaultConfig, commandLineConfig)
+import Snap.Internal.Http.Server.Config (Config (accessLog, errorLog), ConfigLog (ConfigIoLog))
+
+--TODO: Add a link to a large explanation of the idea of using 'def'
+-- | Configure the operation of the Obelisk backend.  For reasonable defaults,
+-- use 'def'.
+data BackendConfig = BackendConfig
+
+instance Default BackendConfig where
+  def = BackendConfig
+
+-- | Start an Obelisk backend
+backend :: BackendConfig -> IO ()
+backend _ = do
+  -- Make output more legible by decreasing the likelihood of output from
+  -- multiple threads being interleaved
+  hSetBuffering stderr LineBuffering
+
+  -- Get the web server configuration from the command line
+  cmdLineConf <- commandLineConfig defaultConfig
+  let httpConf = cmdLineConf
+        { accessLog = Just $ ConfigIoLog BSC8.putStrLn
+        , errorLog = Just $ ConfigIoLog BSC8.putStrLn
+        }
+  -- Start the web server
+  httpServe httpConf $ serveAssets "static.assets" "static"
