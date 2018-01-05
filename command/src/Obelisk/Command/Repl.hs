805a97f4282c2fb06a6d51c2cb3ae7c805da29b2
hsloan <ishaqsloan@gmail.com>
2018-01-05 15:43:40 -0500

    handle case: if user attempts to use 'ob repl' from project sub-directories & outside of project directories

--- command/src/Obelisk/Command/Repl.hs
+++ command/src/Obelisk/Command/Repl.hs
@@ -2,12 +2,21 @@ module Obelisk.Command.Repl where
 
 import Data.Monoid ((<>))
 import System.Process 
+import System.Directory
 
--- TODO modify the nix-shell arguments to recognize when the common dir's files have changed as well. 
+import Obelisk.Command.Project
+
+-- TODO modify the nix-shell --run to recognize when the common dir's files have changed as well. 
 runRepl :: FilePath -> IO ()
-runRepl dir = callProcess "nix-shell" 
-  [ "-A" 
-  , "shells.ghc"
-  , "--run"
-  , "\'cd " <> dir <> "; ghcid -W -c\"cabal new-repl exe:" <> dir <> "\"\'"
-  ]
+runRepl dir = do
+  projectRoot <- findProjectObeliskCommand "."
+  case projectRoot of 
+       Nothing -> putStrLn "Project directory not found."
+       Just pr -> do
+         setCurrentDirectory pr
+         callProcess "nix-shell" 
+            [ "-A" 
+            , "shells.ghc"
+            , "--run"
+            , "\'cd " <> dir <> "; ghcid -W -c\"cabal new-repl exe:" <> dir <> "\"\'"
+            ]
