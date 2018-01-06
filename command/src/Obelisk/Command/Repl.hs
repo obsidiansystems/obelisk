65428204c0d1058573d1ade17ee4c69d37940aa6
hsloan <ishaqsloan@gmail.com>
2018-01-06 18:42:21 -0500

    make use of 'proc' instead of 'shell'

--- command/src/Obelisk/Command/Repl.hs
+++ command/src/Obelisk/Command/Repl.hs
@@ -17,12 +17,8 @@ runRepl dir = do
   findProjectRoot "." >>= \case
      Nothing -> putStrLn "'ob repl' must be used inside of an Obelisk project."
      Just pr -> do
-       createProcess_ "Error: could not create terminal spawn"
-          (shell ghcRepl)
-          {cwd = Just pr} >>= \case 
-             (_, _, _, ph) -> do 
-                waitForProcess ph 
-                return ()
-             _ -> return ()
+       (_, _, _, ph) <- createProcess_ "runRepl" (proc pr ghcRepl) 
+       waitForProcess ph 
+       return ()
   where
-    ghcRepl = "nix-shell -A shells.ghc --run 'cd " <> dir <> "; ghcid -W -c\"cabal new-repl exe:" <> dir <> "\"'" :: String
+    ghcRepl = ["nix-shell -A shells.ghc --run 'cd " <> dir <> "; ghcid -W -c\"cabal new-repl exe:" <> dir <> "\"'"]
