d153604d2d17d394ee73681640c0dfdf365fedf5
hsloan <ishaqsloan@gmail.com>
2018-01-06 11:54:38 -0500

    close all handles, print hout

--- command/src/Obelisk/Command/Repl.hs
+++ command/src/Obelisk/Command/Repl.hs
@@ -17,8 +17,11 @@ runRepl dir = do
   findProjectRoot "." >>= \case
      Nothing -> putStrLn "'ob repl' must be used inside of an Obelisk project."
      Just pr -> do
-       (_, Just hout, _, _) <- createProcess_ "Error: could not create terminal spawn" (shell ghcRepl){ cwd = Just pr, std_out = CreatePipe}
-       --TODO: Read hout handle
-       hClose hout
+       (Just hin, Just hout, Just herr, ph) <- createProcess_ "Error: could not create terminal spawn" 
+          (shell ghcRepl)
+          { cwd = Just pr, std_out = CreatePipe}
+       hShow hout
+       mapM hClose [hin, hout, herr]
+       return ()
   where
     ghcRepl = "nix-shell -A shells.ghc --run cd " <> dir <> "; ghcid -W -c\"cabal new-repl exe:" <> dir <> "\"" :: String
