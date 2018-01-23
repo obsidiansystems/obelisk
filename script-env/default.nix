d0b43f4802dd1792180112c6b5b0341a54f57955
hsloan <ishaqsloan@gmail.com>
2018-01-23 01:25:00 +0000

    remove script-env (stale)

--- script-env/default.nix
+++
@@ -1,5 +0,0 @@
-{ compiler ? "ghc802" }:
-let tryReflex = import ../reflex-platform {};
-in { pkgs = tryReflex.nixpkgs.pkgs;
-     ghcWithPackages = f: tryReflex.nixpkgs.pkgs.haskell.packages.${compiler}.ghcWithPackages (hkgs: with hkgs; [ ghcid ] ++ f hkgs);
-}
--- script-env/ob-init.nix
+++
@@ -1,9 +0,0 @@
-with import ./. {};
-
-let ghc = ghcWithPackages
-     (hkgs: with hkgs; [ text ]);
-in pkgs.stdenv.mkDerivation {
-    name = "generate-env";
-    buildInputs = [ ghc pkgs.cabal2nix ];
-    shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
-}
