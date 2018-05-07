c4a5fa989bc8dff13cf3d14d45c8c7a762df7965
Luigy Leon <luigy@outlook.com>
2018-05-07 11:15:22 -0400

    Move run to it's own module

--- lib/command/src/Obelisk/Command.hs
+++ lib/command/src/Obelisk/Command.hs
@@ -36,58 +36,7 @@ import System.Posix.Process
 import Obelisk.Command.Project
 import Obelisk.Command.Thunk
 import Obelisk.Command.Repl
-
-run :: IO ()
-run = do
-  freePort <- getFreePort
-  pkgs <- getLocalPkgs
-  (pkgDirErrs, hsSrcDirs) <- fmap partitionEithers $ forM pkgs $ \pkg -> do
-    let cabalFp = pkg </> pkg <.> "cabal"
-    xs <- parseHsSrcDir cabalFp
-    return $ case xs of
-      Nothing -> Left pkg
-      Just hsSrcDirs -> Right $ toList $ fmap (pkg </>) hsSrcDirs
-  when (null hsSrcDirs) $
-    fail $ "No valid pkgs found in " <> intercalate ", " pkgs
-  when (not (null pkgDirErrs)) $
-    putStrLn $ "Failed to find pkgs in " <> intercalate ", " pkgDirErrs
-  let dotGhci = unlines
-        [ ":set args --quiet --port " <> show freePort
-        , ":set -i" <> intercalate ":" (mconcat hsSrcDirs)
-        , ":add Backend Frontend"
-        , ":module + Control.Concurrent Obelisk.Widget.Run Frontend Backend"
-        ]
-      testCmd = unlines
-        [ "backendId <- forkIO backend"
-        , "let conf = defRunConfig { _runConfig_redirectPort = " <> show freePort <> "}"
-        , "runWidget conf frontend"
-        , "killThread backendId"
-        ]
-  withSystemTempDirectory "ob-ghci" $ \fp -> do
-    let dotGhciPath = fp </> ".ghci"
-    writeFile dotGhciPath dotGhci
-    runDev dotGhciPath $ Just testCmd
-
--- | Relative paths to local packages of an obelisk project
--- TODO a way to query this
-getLocalPkgs :: IO [FilePath]
-getLocalPkgs = return ["backend", "common", "frontend"]
-
-parseHsSrcDir :: FilePath -- ^ package cabal file path
-              -> IO (Maybe (NE.NonEmpty FilePath)) -- ^ List of hs src dirs of the library component
-parseHsSrcDir cabalFp = do
-  exists <- doesFileExist cabalFp
-  if exists
-    then do
-      withUTF8FileContents cabalFp $ \cabal -> do
-      case parseGenericPackageDescription cabal of
-        ParseOk warnings gpkg -> do
-          mapM_ print warnings
-          return $ do
-            (_, lib) <- simplifyCondTree (const $ pure True) <$> condLibrary gpkg
-            pure $ fromMaybe (pure ".") $ NE.nonEmpty $ hsSourceDirs $ libBuildInfo lib
-        ParseFailed _ -> return Nothing
-    else return Nothing
+import Obelisk.Command.Run
 
 data Args = Args
   { _args_noHandOffPassed :: Bool
--- lib/command/src/Obelisk/Command/Repl.hs
+++ lib/command/src/Obelisk/Command/Repl.hs
@@ -1,16 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Obelisk.Command.Repl
   ( runRepl
-  , runDev
   , watch
-  , getFreePort
   ) where
 
-import qualified Control.Exception as E
-import Data.Maybe
 import Data.Monoid ((<>))
-import Network.Socket
-import System.Process (callCommand)
 
 import Obelisk.Command.Project
 
@@ -21,23 +15,4 @@ runRepl dir = inProjectShell "ghc" $ "cd " <> dir <> "; cabal new-repl"
 -- | Watch the given directory for errors and warnings
 watch :: FilePath -> IO ()
 watch dir = inProjectShell "ghc" $ "cd " <> dir <> "; ghcid -W --command='cabal new-repl'"
-
--- | Dev
-runDev :: FilePath -> Maybe String -> IO ()
-runDev dotGhci mcmd = callCommand $ unwords $ "ghcid" : ghcidOpts
-  where
-    ghcidOpts =
-      [ "-W"
-      , "--command='ghci -ghci-script " <> dotGhci <> "' "
-      ] <> maybeToList (flip fmap mcmd $ \cmd -> " --test=$'" <> cmd <> "'")
-
-getFreePort :: IO PortNumber
-getFreePort = withSocketsDo $ do
-  addr:_ <- getAddrInfo (Just defaultHints) (Just "127.0.0.1") (Just "0")
-  E.bracket (open addr) close socketPort
-  where
-    open addr = do
-      sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-      bind sock (addrAddress addr)
-      return sock
 
