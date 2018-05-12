736f708ae52195d56bc49c25bb54507f2121f29d
Sridhar Ratnakumar <srid@srid.ca>
2018-05-12 15:25:52 -0400

    Colored spinner

--- lib/command/src/Obelisk/CLI/Spinner.hs
+++ lib/command/src/Obelisk/CLI/Spinner.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-module Obelisk.CLI.Spinner where
+module Obelisk.CLI.Spinner (withSpinner') where
 
 import Control.Concurrent (forkIO, killThread, threadDelay)
 import Control.Monad (forM_)
@@ -10,6 +10,8 @@ import Control.Monad.Log (MonadLog, logMessage, runLoggingT)
 import Control.Monad.Reader (MonadIO)
 import Data.Text (Text)
 import qualified Data.Text as T
+import System.Console.ANSI (Color (Magenta), ColorIntensity (Vivid), ConsoleLayer (Foreground),
+                            SGR (Reset, SetColor), setSGRCode)
 
 import Obelisk.CLI.Logging (LoggingConfig, Output (..), handleLog)
 
@@ -22,13 +24,41 @@ withSpinner' c s = bracket run cleanup . const
   where
     -- TODO: Can we obviate passing LoggingConfig just to use forkIO?
     run = liftIO $ forkIO $ flip runLoggingT (handleLog c) $ do
-      forM_ (cycle spinner1) $ \e -> do
-        logMessage $ Output_Raw ["\r", [e], " ", T.unpack s]
+      forM_ (coloredSpinner spinnerPoint) $ \e -> do
+        logMessage $ Output_Raw ["\r", e, " ", T.unpack s]
         delay
     cleanup tid = do
       liftIO $ killThread tid
       logMessage Output_ClearLine
     delay = liftIO $ threadDelay 100000  -- A shorter delay ensures that we update promptly.
 
-spinner1 :: String
-spinner1 = "◐◓◑◒"
+type Spinner = [String]
+
+spinner :: SpinnerTheme -> Spinner
+spinner = cycle
+
+coloredSpinner :: SpinnerTheme -> Spinner
+coloredSpinner = spinner . fmap withColor
+  where
+    withColor s = mconcat
+      [ setSGRCode [SetColor Foreground Vivid color]
+      , s
+      , setSGRCode [Reset]
+      ]
+    color = Magenta
+
+-- Some spinners from https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
+
+type SpinnerTheme = [String]
+
+spinnerPoint :: SpinnerTheme
+spinnerPoint =
+  [ "∙∙∙"
+  , "●∙∙"
+  , "∙●∙"
+  , "∙∙●"
+  , "∙∙∙"
+  ]
+
+-- spinnerCircleHalves :: SpinnerTheme
+-- spinnerCircleHalves = ["◐", "◓", "◑", "◒"]
