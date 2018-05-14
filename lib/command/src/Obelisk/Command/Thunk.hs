a55abddc9f4c2aed8eb179a74f94296bd3127fb6
Luigy Leon <luigy@outlook.com>
2018-05-14 18:47:19 -0400

    Improve missing upstream error
    by displaying available upstreams when the requested one is not found

--- lib/command/src/Obelisk/Command/Thunk.hs
+++ lib/command/src/Obelisk/Command/Thunk.hs
@@ -546,7 +546,7 @@ getThunkPtr thunkDir upstream = do
     remotes <- lines <$> readProcess "hub" [ "-C", thunkDir, "remote" ] ""
     --Check that the upstream specified actually exists
     case L.find (== upstream) remotes of
-      Nothing -> failWith $ T.pack $ "thunk pack: upstream " <> upstream <> " does not exist"
+      Nothing -> failWith $ T.pack $ "thunk pack: upstream " <> upstream <> " does not exist. Available upstreams are " <> L.intercalate ", " remotes
       Just _ -> return ()
     -- iterate over cartesian product
     forM_ repoHeads $ \hd -> do
