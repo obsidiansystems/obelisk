9eb32fbd51209ade534a6e5bf58ea1df689469e4
Tom Smalley <tfsmalley@gmail.com>
2018-05-17 19:07:33 +0100

    Test ob run hot route reloading

--- lib/selftest/src/Obelisk/SelfTest.hs
+++ lib/selftest/src/Obelisk/SelfTest.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Obelisk.SelfTest where
 
+import Control.Monad.IO.Class
 import Control.Exception (throw)
 import Control.Monad
 import Data.Function (fix)
@@ -15,6 +16,7 @@ import qualified Network.HTTP.Types as HTTP
 import Shelly
 import System.Environment
 import System.Exit (ExitCode(..))
+import System.IO (Handle)
 import System.IO.Temp
 import System.Timeout
 import Test.Hspec
@@ -23,7 +25,6 @@ data ObRunState
   = ObRunState_Init
   | ObRunState_Startup
   | ObRunState_BackendStarted Text -- Port of backend
-  | ObRunState_FrontendStarted Text Text -- Port of backend, URI of frontend
   deriving (Eq, Show)
 
 main :: IO ()
@@ -43,24 +44,58 @@ main = do
           run "nix-build" ["--no-out-link", "-A", "ghcjs.frontend"]
 
         it "can use ob run" $ inProj $ handle_sh (\case ExitSuccess -> pure (); e -> throw e) $ do
-          runHandle "ob" ["run"] $ \stdout -> flip fix ObRunState_Init $ \loop -> \case
-            ObRunState_FrontendStarted backendPort frontendUri -> do
-              let req uri = liftIO $ HTTP.parseRequest (T.unpack uri) >>= flip HTTP.httpLbs httpManager
-              req frontendUri >>= \r -> when (HTTP.responseStatus r /= HTTP.ok200) $ errorExit $
-                "Request to frontend server failed: " <> T.pack (show r)
-              void $ req $ "http://localhost:" <> backendPort
-              liftIO (timeout 1000000 $ T.hGetLine stdout) >>= \case
-                Just t | "127.0.0.1" `T.isPrefixOf` t -> exit 0 -- Backend request successfully logged
-                x -> errorExit $ "Couldn't get backend request log: " <> T.pack (show x)
-            last -> liftIO (T.hGetLine stdout) >>= \t -> case last of
-              ObRunState_Init
-                | "Running test..." `T.isPrefixOf` t -> loop ObRunState_Startup
-              ObRunState_Startup
-                | Just port <- "Backend running on port " `T.stripPrefix` t
-                -> loop $ ObRunState_BackendStarted port
-                | not (T.null t) -> errorExit t -- If theres any other output here, startup failed
-              ObRunState_BackendStarted port
-                | Just uri <- "Frontend running on " `T.stripPrefix` t
-                -> loop $ ObRunState_FrontendStarted port uri
-                | not (T.null t) -> errorExit t -- If theres any other output here, startup failed
-              _ -> loop last
+          runHandle "ob" ["run"] $ \stdout -> do
+            (_, firstUri) <- obRun httpManager stdout
+            let newUri = "http://localhost:8080/" -- trailing slash required for comparison
+            when (firstUri == newUri) $ errorExit $
+              "Default URI (" <> firstUri <> ") is the same as test URI (" <> newUri <> ")"
+            alterRouteTo newUri stdout
+            (_, runningUri) <- obRun httpManager stdout
+            if runningUri /= newUri
+            then errorExit $ "Reloading failed: expected " <> newUri <> " but got " <> runningUri
+            else exit 0
+
+-- | Blocks until a non-empty line is available
+hGetLineSkipBlanks :: MonadIO m => Handle -> m Text
+hGetLineSkipBlanks h = liftIO $ fix $ \loop -> T.hGetLine h >>= \case
+  "" -> loop
+  t -> pure t
+
+-- | Alters the route file and waits for `ob run` to reload
+alterRouteTo :: Text -> Handle -> Sh ()
+alterRouteTo uri stdout = do
+  writefile "config/common/route" uri
+  hGetLineSkipBlanks stdout >>= \t -> when (t /= "Reloading...") $ errorExit $
+    "Reloading failed: " <> T.pack (show t)
+  hGetLineSkipBlanks stdout >>= \t -> when (t /= "  config/common/route") $ errorExit $
+    "Reloading failed: " <> T.pack (show t)
+  hGetLineSkipBlanks stdout >>= \t -> when (t /= "Interrupted.") $ errorExit $
+    "Reloading failed: " <> T.pack (show t)
+
+-- | Handle stdout of `ob run`: check that the frontend and backend servers are started correctly
+obRun :: HTTP.Manager -> Handle -> Sh (Text, Text)
+obRun httpManager stdout = flip fix ObRunState_Init $ \loop state -> do
+  liftIO (T.hGetLine stdout) >>= \t -> case state of
+    ObRunState_Init
+      | "Running test..." `T.isPrefixOf` t -> loop ObRunState_Startup
+    ObRunState_Startup
+      | t == "backend stopped; make a change to your code to reload" -> loop ObRunState_Startup
+      | Just port <- "Backend running on port " `T.stripPrefix` t -> loop $ ObRunState_BackendStarted port
+      | not (T.null t) -> errorExit $ "Startup: " <> t -- If theres any other output here, startup failed
+    ObRunState_BackendStarted port
+      | Just uri <- "Frontend running on " `T.stripPrefix` t -> do
+        obRunCheck httpManager stdout port uri
+        pure (port, uri)
+      | not (T.null t) -> errorExit $ "Started: " <> t -- If theres any other output here, startup failed
+    _ -> loop state
+
+-- | Make requests to frontend/backend servers to check they are working properly
+obRunCheck :: HTTP.Manager -> Handle -> Text -> Text -> Sh ()
+obRunCheck httpManager stdout backendPort frontendUri = do
+  let req uri = liftIO $ HTTP.parseRequest (T.unpack uri) >>= flip HTTP.httpLbs httpManager
+  req frontendUri >>= \r -> when (HTTP.responseStatus r /= HTTP.ok200) $ errorExit $
+    "Request to frontend server failed: " <> T.pack (show r)
+  void $ req $ "http://localhost:" <> backendPort
+  liftIO (timeout 1000000 $ T.hGetLine stdout) >>= \case
+    Just t | "127.0.0.1" `T.isPrefixOf` t -> pure () -- Backend request logged
+    x -> errorExit $ "Couldn't get backend request log: " <> T.pack (show x)
