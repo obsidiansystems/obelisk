1585b38cf5fc0da72639b3a0b7e64f50004b5fd1
Luigy Leon <luigy@outlook.com>
2018-05-23 13:24:13 -0400

    Add git ls-remote utils

--- lib/command/src/Obelisk/Command/Utils.hs
+++ lib/command/src/Obelisk/Command/Utils.hs
@@ -1,16 +1,28 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternGuards #-}
 module Obelisk.Command.Utils where
 
-import Control.Applicative (liftA2)
+import Control.Applicative hiding (many)
+import Control.Monad.Except
 import Data.Bool (bool)
+import qualified Text.Megaparsec.Char.Lexer as L
+import Data.Bifunctor
+import Data.Char
+import Data.Either
 import Data.Semigroup ((<>))
+import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Void (Void)
 import System.Directory (canonicalizePath)
 import System.Environment (getExecutablePath)
 import qualified System.Process as P
+import Text.Megaparsec as MP
+import Text.Megaparsec.Char as MP
 
 import Obelisk.App (MonadObelisk)
 import Obelisk.CliApp
@@ -80,3 +92,72 @@ readProc = readProcessAndLogStderr Error
 
 tshow :: Show a => a -> Text
 tshow = T.pack . show
+
+-- like gitLsRemote, but also returns the ref branch if present
+-- NOTE: getting a branch for a symref like HEAD relies on a minimum of git 1.8.5
+gitLsRemoteHEAD :: String -> IO (Either String (CommitId, Maybe GitRef))
+gitLsRemoteHEAD repository = runExceptT $ do
+  let symref = GitRef_Head
+  xs <- ExceptT $ gitLsRemote repository symref
+  let (commits, refs) = bimap M.fromList M.fromList $ partitionEithers $ flip fmap xs $ \case
+        (r, Left commitId) -> Left (r, commitId)
+        (r, Right ref) -> Right (r, ref)
+  case M.lookup symref commits of
+    Nothing -> fail $ "gitLsRemoteHead: Did not find " <> T.unpack (showGitRef symref) <> " in the refs of " <> repository
+    Just commitId -> return (commitId, M.lookup symref refs)
+
+gitLsRemote :: String -> GitRef -> IO (Either String [(GitRef, LsRemoteResult)])
+gitLsRemote repository ref = do
+  res <- P.readProcess "git" ["ls-remote", "--symref", repository, T.unpack $ showGitRef ref] mempty
+  let t = T.pack res
+  return $ first (MP.parseErrorPretty' t) $ MP.runParser (many parseLsRemote) "" t
+
+lexeme :: Parsec Void Text a -> Parsec Void Text a
+lexeme = L.lexeme space
+
+-- $ git ls-remote --symref git@github.com:obsidiansystems/obelisk.git HEAD
+-- ref: refs/heads/master	HEAD
+-- d0a8d25dc93f0acd096bc4ff2f550da9e2d0c8f5	refs/heads/master
+parseLsRemote :: Parsec Void Text (GitRef, LsRemoteResult)
+parseLsRemote = try parseRef <|> parseCommit
+  where
+    parseRef, parseCommit :: Parsec Void Text (GitRef, LsRemoteResult)
+    parseRef = do
+      _ <- lexeme "ref:"
+      ref <- lexeme $ MP.takeWhileP (Just "ref")
+        $ not . isSpace
+      symbolicRef <- lexeme $ MP.takeWhileP (Just "symbolic ref")
+        $ not . isSpace
+      _ <- MP.eol
+      return (toGitRef symbolicRef, Right $ toGitRef ref)
+    parseCommit = do
+      commitId <- lexeme $ MP.takeWhileP (Just "commit id")
+        $ not . isSpace
+      ref <- lexeme $ MP.takeWhileP (Just "ref")
+        $ not . isSpace
+      _ <- MP.eol
+      return (toGitRef ref, Left commitId)
+
+data GitRef
+  = GitRef_Head
+  | GitRef_Branch Text
+  | GitRef_Tag Text
+  | GitRef_Other Text
+  deriving (Show, Eq, Ord)
+
+showGitRef :: GitRef -> Text
+showGitRef = \case
+  GitRef_Head -> "HEAD"
+  GitRef_Branch x -> "refs/heads/" <> x
+  GitRef_Tag x -> "refs/tags/" <> x
+  GitRef_Other x -> x
+
+toGitRef :: Text -> GitRef
+toGitRef r =
+  if | "HEAD" == r -> GitRef_Head
+     | Just s <- "refs/heads/" `T.stripPrefix` r -> GitRef_Branch s
+     | Just s <- "refs/tags/" `T.stripPrefix` r -> GitRef_Tag s
+     | otherwise -> GitRef_Other r
+
+type CommitId = Text
+type LsRemoteResult = Either CommitId GitRef
