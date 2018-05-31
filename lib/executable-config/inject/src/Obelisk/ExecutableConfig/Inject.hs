83be99b45f3de02d1d472483d991a70ea81778c7
Sridhar Ratnakumar <srid@srid.ca>
2018-05-31 15:00:11 -0400

    Exception handling for injection and missing injections

--- lib/executable-config/inject/src/Obelisk/ExecutableConfig/Inject.hs
+++ lib/executable-config/inject/src/Obelisk/ExecutableConfig/Inject.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 module Obelisk.ExecutableConfig.Inject where
 
+import Control.Exception (Exception, throw)
+import Control.Monad (unless)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.Semigroup ((<>))
@@ -14,14 +19,26 @@ import System.FilePath ((</>))
 
 import Obelisk.ExecutableConfig.Types
 
+data InjectionDisallowed = forall config. ObeliskConfig config
+  => InjectionDisallowed (ConfigLocation config)
+
+deriving instance Show InjectionDisallowed
+instance Exception InjectionDisallowed
+
 inject :: forall config. ObeliskConfig config => config -> IO ByteString
 inject cfg = do
+  let loc = configLocation @config
+  unless (canInject loc) $
+    throw $ InjectionDisallowed loc
   fmap snd $ renderStatic $
     elAttr "script" ("type" =: "text/plain" <> "id" =: key) $
       text $ encodeConfig cfg
   where
-    key = case configLocation :: ConfigLocation config of
-      ConfigLocation CabalProject_Backend _ ->
-        error "not allowed" -- XXX: check this compile time
-      _ ->
-        "injected-" <> T.pack (getConfigPath @config)
+    key = "injected-" <> T.pack (getConfigPath @config)
+
+-- | Check if we are allowed to inject the given config into the DOM.
+-- TODO: Can we perform this check at the compile time?
+canInject :: ObeliskConfig config => ConfigLocation config -> Bool
+canInject = \case
+  ConfigLocation CabalProject_Backend _ -> False
+  _ -> True
--- lib/executable-config/lookup/src-ghcjs/Obelisk/ExecutableConfig.hs
+++ lib/executable-config/lookup/src-ghcjs/Obelisk/ExecutableConfig.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE TypeApplications #-}
 module Obelisk.ExecutableConfig where
 
+import Control.Exception (Exception, throw)
+import Control.Monad.Trans.Maybe (MaybeT (..), runMaybeT)
 import qualified Data.ByteString.Char8 as BLC
 import qualified Data.ByteString.Lazy as BLS
 import Data.Semigroup ((<>))
@@ -15,11 +17,19 @@ import GHCJS.DOM.NonElementParentNode
 
 import Obelisk.ExecutableConfig.Types
 
+data NoInjectedContent = NoInjectedContent
+  deriving Show
+
+instance Exception NoInjectedContent
+
 get :: forall config. ObeliskConfig config => IO config
 get = do
   let path = getConfigPath @config
-  -- FIXME: Handle Nothing values and them throwM
-  Just doc <- currentDocument
-  Just e <- getElementById doc $ "injected-" <> path
-  content :: Text <- getInnerHTML e
-  decodeConfig $ BLS.fromStrict $ BLC.pack $ T.unpack content
+  mContent <- runMaybeT $ do
+    doc <- MaybeT currentDocument
+    e <- MaybeT $ getElementById doc $ "injected-" <> path
+    getInnerHTML e
+  case mContent of
+    Nothing -> throw NoInjectedContent
+    Just content ->
+      decodeConfig $ BLS.fromStrict $ BLC.pack $ T.unpack content
