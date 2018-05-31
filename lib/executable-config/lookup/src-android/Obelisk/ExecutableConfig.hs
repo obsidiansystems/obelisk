3f4e43d64d99406e987eeb91feba2c310c6046f8
Sridhar Ratnakumar <srid@srid.ca>
2018-05-30 21:48:19 -0400

    Fix android exe-config lookup to compile

--- lib/executable-config/lookup/src-android/Obelisk/ExecutableConfig.hs
+++ lib/executable-config/lookup/src-android/Obelisk/ExecutableConfig.hs
@@ -3,6 +3,7 @@ module Obelisk.ExecutableConfig (get) where
 
 import Control.Exception (bracket)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
@@ -16,7 +17,7 @@ import Obelisk.ExecutableConfig.Types
 get :: forall config. ObeliskConfig config => IO config
 get = bracket getAssets freeAssetManager $ \mgrObj -> do
   mgr <- assetManagerFromJava mgrObj
-  path <- getConfigPath configPath
+  let path = getConfigPath (configPath :: ConfigPath config)
   let open = do
         a <- withCString path $ \fn ->
           assetManager_open mgr fn 3
@@ -24,8 +25,9 @@ get = bracket getAssets freeAssetManager $ \mgrObj -> do
           then Nothing
           else Just a
       close = mapM_ asset_close
-  content <- bracket open close $ mapM $ \asset -> do
+  -- FIXME: handle Nothing
+  Just content <- bracket open close $ mapM $ \asset -> do
     b <- asset_getBuffer asset
     l <- asset_getLength asset
-    fmap T.decodeUtf8 $ BS.packCStringLen (b, fromIntegral l)
-  parseConfig content
+    BS.packCStringLen (b, fromIntegral l)
+  parseConfig $ BSL.fromStrict content
