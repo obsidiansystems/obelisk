7963fbcee1d4d852e2904b679ab6ac40f18f6c51
Sridhar Ratnakumar <srid@srid.ca>
2018-06-18 18:53:04 -0400

    Refactor migration so that command determines the monoid instances
    Also have the path finding algorithm directly returning the mappend'ed action.
    
    This change is first needed in order to memoize the algorithm

--- lib/command/src/Obelisk/Command/Upgrade.hs
+++ lib/command/src/Obelisk/Command/Upgrade.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -9,6 +10,7 @@ import Control.Monad (unless, void)
 import Control.Monad.Catch (onException)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Maybe (MaybeT (..), runMaybeT)
+import Data.Monoid (Any (..), getAny)
 import Data.Semigroup ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -27,6 +29,12 @@ import Obelisk.Command.Thunk (updateThunk)
 
 import Obelisk.Migration
 
+newtype HandOffAction = HandoffAction Any
+  deriving (Monoid, Ord, Eq, Show)
+
+instance Action HandOffAction where
+  parseEdgeMeta = HandoffAction . Any . (== "True")
+
 data MigrationGraph
   = MigrationGraph_ObeliskUpgrade
   | MigrationGraph_ObeliskHandoff
@@ -43,7 +51,6 @@ fromGraphName = \case
   "obelisk-upgrade" -> MigrationGraph_ObeliskUpgrade
   _ -> error "Invalid graph name specified"
 
-
 ensureCleanProject :: MonadObelisk m => FilePath -> m ()
 ensureCleanProject project = ensureCleanGitRepo project False "Cannot upgrade with uncommited changes"
 
@@ -59,26 +66,23 @@ decideHandOffToProjectOb project = do
       False -> do
         putLog Warning "Project ob not found in ambient ob's migration graph; handing off anyway"
         return True
-      True -> findShortestEquivalentPath ambientGraph projectHash ambientHash >>= \case
+      True -> findPathAction ambientGraph projectHash ambientHash >>= \case
         Nothing -> do
           putLog Warning "No migration path between project and ambient ob; handing off anyway"
           return True
-        Just path -> do
-          putLog Debug $ "Found " <> T.pack (show $ length path) <> " edges between " <> projectHash <> " and " <> ambientHash <> " in ambient ob graph"
-          fmap (not . or . fmap parseHandoffMigration) $ traverse (getAction ambientGraph) path
+        Just (HandoffAction dontHandoff) -> do
+          return $ not $ getAny dontHandoff
+          -- fmap (not . or . fmap parseHandoffMigration) $ traverse (getAction ambientGraph) path
   where
     getAmbientObInfo = do
       ambientOb <- getAmbientOb
-      getMigrationGraph ambientOb MigrationGraph_ObeliskHandoff >>= \case
+      getMigrationGraph' ambientOb MigrationGraph_ObeliskHandoff >>= \case
         Nothing -> do
           failWith "Ambient ob has no migration (this can't be possible)"
         Just (m, _, ambientHash) -> do
           unless (hasVertex ambientHash m) $
             failWith "Ambient ob's hash is not in its own graph"
           return (m, ambientHash)
-    parseHandoffMigration = \case
-      "True" -> True
-      _ -> False
 
 -- | Return the path to the current ('ambient') obelisk process Nix directory
 getAmbientOb :: MonadObelisk m => m FilePath
@@ -114,7 +118,7 @@ migrateObelisk :: MonadObelisk m => FilePath -> Hash -> m ()
 migrateObelisk project fromHash = void $ withSpinner' "Migrating to new Obelisk" (Just id) $ do
   updateThunk (toImplDir project) $ \obImpl -> revertObImplOnFail obImpl $ do
     toHash <- computeVertexHash obImpl MigrationGraph_ObeliskUpgrade obImpl
-    (g, _, _) <- getMigrationGraph obImpl MigrationGraph_ObeliskUpgrade >>= \case
+    (g, _, _) <- getMigrationGraph' obImpl MigrationGraph_ObeliskUpgrade >>= \case
       Nothing -> failWith "New obelisk has no migration metadata"
       Just m -> pure m
 
@@ -131,16 +135,13 @@ migrateObelisk project fromHash = void $ withSpinner' "Migrating to new Obelisk"
         pure $ "No upgrade available (new Obelisk is the same)"
       else do
         putLog Debug $ "Migrating from " <> fromHash <> " to " <> toHash
-        runMigration g fromHash toHash >>= \case
+        findPathAction g fromHash toHash >>= \case
           Nothing -> do
             failWith "Unable to find migration path"
-          Just actions -> do
-            let allActions = T.concat $ fmap snd actions
-            let actionsNE = filter (/= mempty) $ fmap snd actions
+          Just action -> do
             putLog Notice $ "Actionable migrations (if any) are shown below:\n"
-            putLog Notice $ allActions
+            putLog Notice $ action
             putLog Notice $ "\nPlease commit the changes to this repo, and manually perform the above migrations if any to migrate your project to the upgraded Obelisk.\n"
-            putLog Debug $ T.pack $ show (length actionsNE) <> " actionable out of " <> show (length actions) <> " migrations"
             pure $ "Migrated from " <> fromHash <> " to " <> toHash
   where
     revertObImplOnFail impl f = f `onException` do
@@ -148,15 +149,17 @@ migrateObelisk project fromHash = void $ withSpinner' "Migrating to new Obelisk"
       callProcessAndLogOutput (Notice, Notice) $ git1 project ["checkout", impl]
 
 -- | Get the migration graph for project, along with the first and last hash.
-getMigrationGraph
-  :: MonadObelisk m => FilePath -> MigrationGraph -> m (Maybe (Migration Text, Hash, Hash))
-getMigrationGraph project graph = runMaybeT $ do
+getMigrationGraph'
+  :: (Action action, MonadObelisk m)
+  => FilePath -> MigrationGraph -> m (Maybe (Migration action, Hash, Hash))
+getMigrationGraph' project graph = runMaybeT $ do
   let name = graphName graph
   putLog Debug $ "Reading migration graph " <> name <> " from " <> T.pack project
-  g <- MaybeT $ liftIO $ readGraph T.pack  (migrationDir project) name
+  g <- MaybeT $ liftIO $ readGraph (migrationDir project) name
   first <- MaybeT $ getFirst $ _migration_graph g
   last' <- MaybeT $ getLast $ _migration_graph g
   pure $ (g, first, last')
+
 
 computeVertexHash :: MonadObelisk m => FilePath -> MigrationGraph -> FilePath -> m Hash
 computeVertexHash obDir graph repoDir = fmap T.pack $ readProcessAndLogStderr Error $
--- lib/migration/src/Obelisk/Migration.hs
+++ lib/migration/src/Obelisk/Migration.hs
@@ -7,7 +7,6 @@ module Obelisk.Migration where
 
 import Control.Monad (forM)
 import Control.Monad.Catch (Exception, MonadThrow, throwM)
-import Data.List (minimumBy)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (catMaybes)
@@ -50,29 +49,21 @@ instance Exception MultipleTerminalVertices
 instance Exception NonEquivalentPaths
 instance Exception MissingEdgeInActionMap
 
+class Action action where
+  parseEdgeMeta :: String -> action
+
+instance Action Text where
+  parseEdgeMeta = T.pack
+
 -- | Get the directed-acylic graph of migration
 getDag :: (MonadThrow m, Ord a) => AdjacencyMap a -> m (AdjacencyMap a)
 getDag g = case topSort g of
   Just _ -> pure g
   Nothing -> throwM CyclicGraphError
 
--- | Return the list of actions that migrate from one hash to another
---
--- Return Nothing if no path can be found.
-runMigration
-  :: (MonadThrow m, Eq action, Ord action, Monoid action)
-  => Migration action
-  -> Hash
-  -> Hash
-  -> m (Maybe [(Hash, action)])
-runMigration m a b = findShortestEquivalentPath m a b >>= traverse (traverse getActionWithHash)
-  where
-    getActionWithHash e@(_, v2) = getAction m e >>= pure . (v2,)
-
 -- | Read the graph from a directory of files.
-readGraph :: (String -> action) -> FilePath -> Text -> IO (Maybe (Migration action))
-readGraph parseAction root name = doesDirectoryExist root >>= \case
-  -- TODO: maybe get rid of parseAction (using typeclass?)
+readGraph :: Action action => FilePath -> Text -> IO (Maybe (Migration action))
+readGraph root name = doesDirectoryExist root >>= \case
   False -> return Nothing
   True -> do
     allVertexPairs <- listDirectory root >>= return . catMaybes . fmap getEdgeVertices
@@ -89,48 +80,39 @@ readGraph parseAction root name = doesDirectoryExist root >>= \case
       let f = root </> (T.unpack $ v1 <> "-" <> v2) </> T.unpack graph
       doesFileExist f >>= \case
         True -> do
-          c <- readFile f >>= pure . parseAction
+          c <- readFile f >>= pure . parseEdgeMeta
           return $ Just c
         False -> return Nothing
 
--- | Find the shortest equivalent path between vertices
+-- TODO: memoize for performance
+-- | Find the concataneted actions between two vertices
 --
--- This path is equivalent to every other path per monoid concat of edge actions.
---
--- Return `Nothing` if no path exists between the vertices, and `Left` if paths
--- are not equivalent. The successful case will be a `Right`
-findShortestEquivalentPath
-  :: (MonadThrow m, Eq action, Ord action, Monoid action)
-  => Migration action -> Hash -> Hash -> m (Maybe Path)
-findShortestEquivalentPath m a b = findAllPaths m a b >>= \case
-  [] -> pure Nothing
-  paths -> verify paths >>= \case
-    True -> pure $ Just $ minimumBy (\x y -> compare (length x) (length y)) paths
-    False -> throwM $ NonEquivalentPaths a b
+-- All paths between the vertices must have the same mappend'ed action;
+-- otherwise this will throw.
+findPathAction
+  :: (MonadThrow m, Action action, Monoid action, Ord action)
+  => Migration action -> Hash -> Hash -> m (Maybe action)
+findPathAction m a b = do
+  dag <- getDag (_migration_graph m)
+  go a b $ adjacencyMap dag
   where
-    verify xs = ((== 1) . length) . uniqs <$> traverse (fmap mconcat . traverse (getAction m)) xs
     uniqs = Set.toList . Set.fromList
-
--- | Find all possible paths from a to b
---
--- Vertices a and b must already exist (as verified by hasVertex).
--- Return empty list if no paths exist.
-findAllPaths :: MonadThrow m => Migration action -> Hash -> Hash -> m [Path]
-findAllPaths m a b = pure . fmap listInPairs . go a b . adjacencyMap =<< getDag (_migration_graph m)
-  where
-    -- Do a slow and dummy search. TODO: improve algorithm performance
     go x y g = case x == y of
       True ->
-        [[y]]
+        pure $ Just mempty
       False -> case Map.lookup x g of
-        Just adjs ->
-          fmap (x :) $ mconcat $ fmap (\z -> go z y g) $ Set.toList adjs
-        Nothing -> []
-    listInPairs = \case
-      [] -> []
-      [_] -> []  -- One vertex path has zero edges (thus, zero length path)
-      [x, y] -> [(x,y)]
-      x:y:xs -> (x,y) : listInPairs (y : xs)
+        Just adjs -> do
+          actions <- fmap catMaybes $ flip traverse (Set.toList adjs) $ \z -> do
+            action0 <- getAction m (x, z)
+            actionM <- go z y g
+            pure $ fmap (mappend action0) $ actionM
+          case uniqs actions of
+            [] -> pure Nothing
+            [v] -> pure $ Just v  -- Exactly one monoidal value; accept.
+            _ -> throwM $ NonEquivalentPaths x y
+        Nothing ->
+          pure Nothing
+
 
 hasVertex :: Hash -> Migration action -> Bool
 hasVertex h (Migration g _) = Set.member h $ vertexSet g
