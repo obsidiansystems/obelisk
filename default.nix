1d26a3d29494780e082782d188675a895a8c427f
3noch <eacameron@gmail.com>
2018-06-27 14:19:52 -0400

    Add `nix-build -A linuxExe` and make -A exe build native

--- default.nix
+++ default.nix
@@ -297,11 +297,14 @@ rec {
                 };
               };
           in mkProject (projectDefinition args));
-      exe = serverExe (projectOut "x86_64-linux").ghc.backend (projectOut system).ghcjs.frontend assets.symlinked configPath;
+      serverOn = sys:
+        serverExe (projectOut sys).ghc.backend (projectOut system).ghcjs.frontend assets.symlinked configPath;
+      linuxExe = serverOn "x86_64-linux";
     in projectOut system // {
-      inherit exe;
-      server = { hostName }: server exe hostName;
-      obelisk = import (base + "/.obelisk/impl") {};
+      inherit linuxExe;
+      exe = serverOn system;
+      server = { hostName }: server linuxExe hostName;
+      obelisk = import (base + /.obelisk/impl) {};
     };
   haskellPackageSets = {
     ghc = reflex-platform.ghc.override {
