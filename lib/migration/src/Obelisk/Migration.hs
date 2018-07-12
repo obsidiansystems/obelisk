288eab4864b3a15b575ede81ac7277ae1c08deda
Robert Klotzner <robert.klotzner@gmx.at>
2018-07-12 17:14:29 +0200

    read files strictly in "Obelisk.Migration".
    To avoid "too many open files" error.

--- lib/migration/src/Obelisk/Migration.hs
+++ lib/migration/src/Obelisk/Migration.hs
@@ -17,6 +17,7 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import System.Directory
 import System.FilePath
 
@@ -66,10 +67,10 @@ instance Exception MissingEdgeInActionMap
 instance Exception NonLinearGraph
 
 class Action action where
-  parseEdgeMeta :: String -> action
+  parseEdgeMeta :: Text -> action
 
 instance Action Text where
-  parseEdgeMeta = T.pack
+  parseEdgeMeta = id
 
 -- | Get the directed-acylic graph of migration
 -- TODO: Consider calling getDag only when reading the graph for efficiency.
@@ -95,7 +96,7 @@ readGraph root name = doesDirectoryExist root >>= \case
       let f = root </> (T.unpack $ v1 <> "-" <> v2) </> T.unpack graph
       doesFileExist f >>= \case
         True -> do
-          c <- readFile f >>= pure . parseEdgeMeta
+          c <- parseEdgeMeta <$> T.readFile f
           return $ Just c
         False -> do
           return Nothing
