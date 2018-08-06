253918954610a7b691cdbc1103c24e85e4e5a830
Ryan Trinkle <ryan.trinkle@gmail.com>
2018-08-06 18:22:26 -0400

    More and better instances for Obelisk.Route.Frontend
    (cherry picked from commit 0c66e0e8381bfb09204ef71b09203b0507231b20)

--- lib/route/src/Obelisk/Route/Frontend.hs
+++ lib/route/src/Obelisk/Route/Frontend.hs
@@ -90,7 +90,7 @@ instance HasJSContext m => HasJSContext (RoutedT t r m) where
 instance Prerender js m => Prerender js (RoutedT t r m) where
   prerenderClientDict = fmap (\Dict -> Dict) (prerenderClientDict :: Maybe (Dict (PrerenderClientConstraint js m)))
 
-instance (Requester t m, PrimMonad m) => Requester t (RoutedT t r m) where
+instance Requester t m => Requester t (RoutedT t r m) where
   type Request (RoutedT t r m) = Request m
   type Response (RoutedT t r m) = Response m
   requesting = RoutedT . requesting
@@ -134,6 +134,11 @@ instance Adjustable t m => Adjustable t (RoutedT t r m) where
   traverseIntMapWithKeyWithAdjust f a0 a' = RoutedT $ traverseIntMapWithKeyWithAdjust (coerce f) (coerce a0) $ coerce a'
   traverseDMapWithKeyWithAdjust f a0 a' = RoutedT $ traverseDMapWithKeyWithAdjust (\k v -> coerce $ f k v) (coerce a0) $ coerce a'
   traverseDMapWithKeyWithAdjustWithMove f a0 a' = RoutedT $ traverseDMapWithKeyWithAdjustWithMove (\k v -> coerce $ f k v) (coerce a0) $ coerce a'
+
+instance (Monad m, MonadQuery t vs m) => MonadQuery t vs (RoutedT t r m) where
+  tellQueryIncremental = lift . tellQueryIncremental
+  askQueryResult = lift askQueryResult
+  queryIncremental = lift . queryIncremental
 
 runRoutedT :: RoutedT t r m a -> Dynamic t r -> m a
 runRoutedT = runReaderT . unRoutedT
