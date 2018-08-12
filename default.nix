2f028482120207f541bcb09f7dcea066c45c4c6a
Ali Abrar <aliabrar@gmail.com>
2018-08-12 11:50:12 -0400

    Symlink all backend executables into serverExe payload

--- default.nix
+++ default.nix
@@ -183,7 +183,7 @@ rec {
     pkgs.runCommand "serverExe" {} ''
       mkdir $out
       set -eux
-      ln -s "${justStaticExecutables backend}"/bin/backend $out/backend
+      ln -s "${justStaticExecutables backend}"/bin/* $out/
       ln -s "${mkAssets assets}" $out/static.assets
       ln -s "${config}" $out/config
       ln -s ${mkAssets (compressedJs frontend)} $out/frontend.jsexe.assets
