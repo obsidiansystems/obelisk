df1aa9d9216753540da8c8667530c8a82c925e42
Ali Abrar <aliabrar@gmail.com>
2018-09-02 15:35:49 -0400

    Update executables to use routes

--- skeleton/backend/src-bin/main.hs
+++ skeleton/backend/src-bin/main.hs
@@ -1,20 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Control.Exception
-import Control.Lens
-import Data.Maybe
-import Data.Text.Encoding
-import Obelisk.ExecutableConfig (get)
-import System.Environment
-import qualified Text.URI as URI
-import Text.URI.Lens
-
 import Backend
+import Frontend
+import Obelisk.Backend
 
 main :: IO ()
-main = do
-  Just route <- get "common/route"
-  port <- case URI.mkURI route of
-    Left err -> fail $ show err
-    Right uri -> return $ fromMaybe 8000 $ uri ^? uriAuthority . _Right . authPort . _Just
-  withArgs ["--port", show port] backend
-
+main = runBackend backend frontend
