b3d9a9bf2055d2eec1715372a8a8ddf48dc16fe5
Ali Abrar <aliabrar@gmail.com>
2018-09-02 10:44:40 -0400

    Update skeleton with routes

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -4,16 +4,19 @@
 module Frontend where
 
 import qualified Data.Text as T
+import Obelisk.Frontend
+import Obelisk.Route
 import Reflex.Dom.Core
 
 import Common.Api
 import Static
 
-frontend :: (StaticWidget x (), Widget x ())
-frontend = (head', body)
-  where
-    head' = el "title" $ text "Obelisk Minimal Example"
-    body = do
+frontend :: Frontend (R IndexOnlyRoute)
+frontend = Frontend
+  { _frontend_head = el "title" $ text "Obelisk Minimal Example"
+  , _frontend_body = do
       text "Welcome to Obelisk!"
       el "p" $ text $ T.pack commonStuff
       elAttr "img" ("src" =: static @"obelisk.jpg") blank
+  , _frontend_notFoundRoute = const $ IndexOnlyRoute :/ ()
+  }
