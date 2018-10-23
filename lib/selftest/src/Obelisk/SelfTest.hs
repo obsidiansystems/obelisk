84b946cd46a75beca273b5b9d893825e72a64d20
Tom Smalley <tfsmalley@gmail.com>
2018-10-23 13:35:18 +0100

    Get free ports upfront in selftest
    Fixes the ob run tests occasionally fighting over ports when run in
    parallel

--- lib/selftest/src/Obelisk/SelfTest.hs
+++ lib/selftest/src/Obelisk/SelfTest.hs
@@ -90,6 +90,7 @@ main = do
   let verbosity = bool silently verbosely isVerbose
   obeliskImpl <- fromString <$> getEnv "OBELISK_IMPL"
   httpManager <- HTTP.newManager HTTP.defaultManagerSettings
+  [p0, p1, p2, p3] <- liftIO $ getFreePorts 4
   withSystemTempDirectory "initCache" $ \initCache -> do
     -- Setup the ob init cache
     void . shellyOb verbosity $ chdir (fromString initCache) $ do
@@ -144,9 +145,9 @@ main = do
       -- when not run by 'nix-build --attr selftest'
       describe "ob run" $ parallel $ do
         it "works in root directory" $ inTmpObInit $ \_ -> do
-          testObRunInDir Nothing httpManager
+          testObRunInDir p0 p1 Nothing httpManager
         it "works in sub directory" $ inTmpObInit $ \_ -> do
-          testObRunInDir (Just "frontend") httpManager
+          testObRunInDir p2 p3 (Just "frontend") httpManager
 
       describe "obelisk project" $ parallel $ do
         it "can build obelisk command"  $ shelly_ $ run "nix-build" ["-A", "command" , obeliskImpl]
@@ -217,14 +218,13 @@ main = do
           testThunkPack (dir </> thunk)
 
 -- | Run `ob run` in the given directory (maximum of one level deep)
-testObRunInDir :: Maybe Shelly.FilePath -> HTTP.Manager -> Sh ()
-testObRunInDir mdir httpManager = handle_sh (\case ExitSuccess -> pure (); e -> throw e) $ do
+testObRunInDir :: Socket.PortNumber -> Socket.PortNumber -> Maybe Shelly.FilePath -> HTTP.Manager -> Sh ()
+testObRunInDir p0 p1 mdir httpManager = handle_sh (\case ExitSuccess -> pure (); e -> throw e) $ do
   let uri p = "http://localhost:" <> T.pack (show p) <> "/" -- trailing slash required for comparison
-  writefile "config/common/route" . uri =<< liftIO getFreePort -- Use a free port initially
+  writefile "config/common/route" $ uri p0
   maybe id chdir mdir $ runHandle "ob" ["run"] $ \stdout -> do
-    freePort <- liftIO getFreePort
     firstUri <- handleObRunStdout httpManager stdout
-    let newUri = uri freePort
+    let newUri = uri p1
     when (firstUri == newUri) $ errorExit $
       "Startup URI (" <> firstUri <> ") is the same as test URI (" <> newUri <> ")"
     maybe id (\_ -> chdir "..") mdir $ alterRouteTo newUri stdout
@@ -299,10 +299,11 @@ obRunCheck httpManager _stdout frontendUri = do
   req frontendUri >>= \r -> when (HTTP.responseStatus r /= HTTP.ok200) $ errorExit $
     "Request to frontend server failed: " <> T.pack (show r)
 
-getFreePort :: IO Socket.PortNumber
-getFreePort = Socket.withSocketsDo $ do
+getFreePorts :: Int -> IO [Socket.PortNumber]
+getFreePorts 0 = pure []
+getFreePorts n = Socket.withSocketsDo $ do
   addr:_ <- Socket.getAddrInfo (Just Socket.defaultHints) (Just "127.0.0.1") (Just "0")
-  bracket (open addr) Socket.close Socket.socketPort
+  bracket (open addr) Socket.close $ \s -> (:) <$> Socket.socketPort s <*> getFreePorts (n - 1)
   where
     open addr = do
       sock <- Socket.socket (Socket.addrFamily addr) (Socket.addrSocketType addr) (Socket.addrProtocol addr)
