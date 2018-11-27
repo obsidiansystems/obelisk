f87aee775be38f931fe0b5bd3c02db3aa24dfe60
Luigy Leon <luichi.19@gmail.com>
2018-11-27 16:38:29 -0500

    Config and route url example

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -3,9 +3,12 @@
 {-# LANGUAGE TypeApplications #-}
 module Frontend where
 
+import Control.Monad.IO.Class
 import qualified Data.Text as T
 import Obelisk.Frontend
 import Obelisk.Route
+import Obelisk.Route.Frontend
+import qualified Obelisk.ExecutableConfig as Config
 import Reflex.Dom.Core
 
 import Common.Api
@@ -17,6 +20,10 @@ frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
   { _frontend_head = el "title" $ text "Obelisk Minimal Example"
   , _frontend_body = do
+      route <- askRoute
+      toUrl <- askRouteToUrl
+      Just host <- liftIO $ Config.get "config/common/route"
+      el "div" $ dynText $ (host <>) . toUrl <$> route
       text "Welcome to Obelisk!"
       el "p" $ text $ T.pack commonStuff
       elAttr "img" ("src" =: static @"obelisk.jpg") blank
