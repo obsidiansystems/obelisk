c9283da0fd31d2d8ed3e659cdb2ec5f74318f1df
Matthew Bauer <mjbauer95@gmail.com>
2019-01-24 15:02:30 -0500

    Add new loader for GitHub
    This can use Nix 2.0’s builtins.fetchTarball if it is available. If
    not, it will fall back on old behavior.

--- lib/command/src/Obelisk/Command/Thunk.hs
+++ lib/command/src/Obelisk/Command/Thunk.hs
@@ -415,8 +415,9 @@ setThunk target gs branch = do
 -- This tool will only ever produce the newest one when it writes a thunk.
 gitHubStandaloneLoaders :: NonEmpty Text
 gitHubStandaloneLoaders =
-  gitHubStandaloneLoaderV2 :|
-  [ gitHubStandaloneLoaderV3
+  gitHubStandaloneLoaderV4 :|
+  [ gitHubStandaloneLoaderV2
+  , gitHubStandaloneLoaderV3
   , gitHubStandaloneLoaderV1
   ]
 
@@ -454,6 +455,31 @@ gitHubStandaloneLoaderV3 = T.unlines
   , "      // (if branch == null then {} else { ref = branch; })"
   , "      // (if name == null then {} else { inherit name; }));"
   , "in import (fetch (builtins.fromJSON (builtins.readFile ./github.json)))"
+  ]
+
+gitHubStandaloneLoaderV4 :: Text
+gitHubStandaloneLoaderV4 = T.unlines
+  [ "# DO NOT HAND-EDIT THIS FILE"
+  , "let"
+  , "  fetch = { private ? false, ... }@args:"
+  , "    if private && builtins.hasAttr \"fetchGit\" builtins"
+  , "    then fetchFromGitHubPrivate args"
+  , "    else fetchFromGitHub (builtins.removeAttrs args [\"branch\"]);"
+  , "  fetchFromGitHub = if builtins.hasAttr \"fetchTarball\" builtins then"
+  , "    ({ owner, repo, rev, sha256, ... }: builtins.fetchTarball {"
+  , "      url = \"https://github.com/${owner}/${repo}/archive/${rev}.tar.gz\";"
+  , "      inherit sha256; })"
+  , "    else (import <nixpkgs> {}).fetchFromGitHub;"
+  , "  fetchFromGitHubPrivate ="
+  , "    { owner, repo, rev, branch ? null, name ? null, sha256 ? null"
+  , "    , private ? false, fetchSubmodules ? false"
+  , "    , githubBase ? \"github.com\", ...}: assert !fetchSubmodules;"
+  , "      builtins.fetchGit ({"
+  , "        url = \"ssh://git@${githubBase}/${owner}/${repo}.git\";"
+  , "        inherit rev;"
+  , "      } // (if branch == null then {} else { ref = branch; })"
+  , "        // (if name == null then {} else { inherit name; }));"
+ , "in import (fetch (builtins.fromJSON (builtins.readFile ./github.json)))"
   ]
 
 plainGitStandaloneLoaders :: NonEmpty Text
