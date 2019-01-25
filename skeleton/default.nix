e298d058fdf26f90a30c1d6e3afda299385cb68b
Tom Smalley <tfsmalley@gmail.com>
2019-01-25 15:24:36 +0000

    Allow configuring closure compiler as command line arg

--- skeleton/default.nix
+++ skeleton/default.nix
@@ -1,5 +1,6 @@
 { system ? builtins.currentSystem # TODO: Get rid of this system cruft
 , iosSdkVersion ? "10.2"
+, closureCompilerOptimizationLevel ? "ADVANCED" # Set this to `null` to skip the closure compiler step
 }:
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
 project ./. ({ ... }: {
@@ -7,4 +8,5 @@ project ./. ({ ... }: {
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+  __closureCompilerOptimizationLevel = closureCompilerOptimizationLevel;
 })
