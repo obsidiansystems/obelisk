4baf8a9eabb8fa72e99e33a2a5b073d188ee3e11
Sridhar Ratnakumar <srid@srid.ca>
2019-03-20 16:57:31 -0400

    Make ExecutableConfig.get use a dev local override config

--- lib/executable-config/lookup/src/Obelisk/ExecutableConfig.hs
+++ lib/executable-config/lookup/src/Obelisk/ExecutableConfig.hs
@@ -1,13 +1,21 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Obelisk.ExecutableConfig (get) where
 
 import Control.Exception
+import Data.Bool (bool)
 import Data.Text (Text)
-import Data.Text as T
-import Data.Text.IO as T
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import System.Directory (doesFileExist)
 import System.FilePath.Posix ((</>))
 import System.IO.Error
 
 get :: Text -> IO (Maybe Text)
 get path = do
-  let doesNotExist = \e -> if isDoesNotExistError e then Just () else Nothing
-  catchJust doesNotExist (fmap Just $ T.readFile $ T.unpack path) (\_ -> pure Nothing)
+  path' <- override path (path <> ".dev")
+  catchJust doesNotExist (fmap Just $ T.readFile $ T.unpack path') (\_ -> pure Nothing)
+  where
+    -- Return p1 if it exists; otherwise return p.
+    override p p1 = bool p p1 <$> doesFileExist (T.unpack p1)
+    doesNotExist = \e -> if isDoesNotExistError e then Just () else Nothing
