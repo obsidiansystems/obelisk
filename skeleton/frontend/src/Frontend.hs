3cab0183979898a3e27b11a852d854076753a65f
James Deikun <james.deikun@obsidian.systems>
2019-04-04 14:34:56 -0400

    test scenario in skeleton

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -3,8 +3,10 @@
 {-# LANGUAGE TypeApplications #-}
 module Frontend where
 
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import Obelisk.Frontend
+import Obelisk.Frontend.Config
 import Obelisk.Route
 import Reflex.Dom.Core
 
@@ -18,6 +20,8 @@ frontend = Frontend
   { _frontend_head = el "title" $ text "Obelisk Minimal Example"
   , _frontend_body = do
       text "Welcome to Obelisk!"
+      greeting <- getConfig "config/frontend/greeting"
+      el "p" $ text $ fromMaybe "I forgot my line ... ~\\_(O_o)_/~" greeting
       el "p" $ text $ T.pack commonStuff
       elAttr "img" ("src" =: static @"obelisk.jpg") blank
   }
