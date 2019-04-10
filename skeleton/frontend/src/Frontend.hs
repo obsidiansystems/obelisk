ff6ac41768315fead852eba4e09749b7d0e22f4e
James Deikun <james.deikun@obsidian.systems>
2019-04-10 16:51:41 -0400

    try to get greeting both ways

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -3,8 +3,11 @@
 {-# LANGUAGE TypeApplications #-}
 module Frontend where
 
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Class (lift)
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
+import qualified Obelisk.ExecutableConfig as EC
 import Obelisk.Frontend
 import Obelisk.Frontend.Config
 import Obelisk.Route
@@ -20,8 +23,10 @@ frontend = Frontend
   { _frontend_head = el "title" $ text "Obelisk Minimal Example"
   , _frontend_body = do
       text "Welcome to Obelisk!"
-      greeting <- getConfig "config/frontend/greeting"
-      el "p" $ text $ fromMaybe "I forgot my line ... ~\\_(O_o)_/~" greeting
+      greeting1 <- lift $ liftIO $ EC.get "config/frontend/greeting"
+      el "p" $ text $ "old way: " <> fromMaybe "I forgot my line ... ~\\_(O_o)_/~" greeting1
+      greeting2 <- getConfig "config/frontend/greeting"
+      el "p" $ text $ "new way: " <> fromMaybe "I forgot my line ... ~\\_(O_o)_/~" greeting2
       el "p" $ text $ T.pack commonStuff
       elAttr "img" ("src" =: static @"obelisk.jpg") blank
   }
