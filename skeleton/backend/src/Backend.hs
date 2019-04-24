cfe31f7fc71dca5ae98734e8e751c9ba9bf2ec33
Doug Beardsley <mightybyte@gmail.com>
2019-04-24 01:43:26 -0400

    Add missing type parameters

--- skeleton/backend/src/Backend.hs
+++ skeleton/backend/src/Backend.hs
@@ -9,7 +9,7 @@ import Common.Route
 import Obelisk.Backend
 import Snap.Http.Server
 
-backend :: Backend BackendRoute FrontendRoute
+backend :: Backend BackendRoute FrontendRoute Snap AppConfig
 backend = Backend
   { _backend_routeEncoder = backendRouteEncoder
   , _backend_routes = const $ return ()
