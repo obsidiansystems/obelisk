260bc41a650d09d17fa951496a95f8cbf7e746b5
Cale Gibbard <cgibbard@gmail.com>
2019-05-28 14:25:01 -0400

    tabulation: Add default implementation for traverseWithField in terms of tabulateFieldsA, add MINIMAL pragma.

--- lib/tabulation/src/Data/Tabulation.hs
+++ lib/tabulation/src/Data/Tabulation.hs
@@ -8,10 +8,14 @@ import Control.Lens
 -- | This is a class for record types whose fields can be enumerated by an associated GADT. It's closely related to the concept of a representable functor, except without the functor part, and the fields are not all the same type.
 class HasFields a where
   type Field a :: * -> *
+  
+  fieldLens :: Field a x -> Lens' a x
+  tabulateFieldsA :: Applicative f => (forall x. Field a x -> f x) -> f a
+
   tabulateFields :: (forall x. Field a x -> x) -> a
   tabulateFields f = runIdentity (tabulateFieldsA (Identity . f))
-  tabulateFieldsA :: Applicative f => (forall x. Field a x -> f x) -> f a
-  fieldLens :: Field a x -> Lens' a x
   traverseWithField :: Applicative m => (forall x. Field a x -> x -> m x) -> a -> m a
+  traverseWithField t r = tabulateFieldsA (\f -> t f (indexField r f))
   indexField :: a -> Field a x -> x
   indexField a f = a ^. fieldLens f
+  {-# MINIMAL fieldLens, tabulateFieldsA #-}
