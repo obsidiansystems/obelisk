911ee90aaa5b2e2524c5d6f10e69db9c3fbefcda
Ali Abrar <aliabrar@gmail.com>
2019-06-15 12:21:56 -0400

    executable-config: Fix up internal directory module

--- lib/executable-config/lookup/src/Obelisk/Configs/Internal/Directory.hs
+++ lib/executable-config/lookup/src/Obelisk/Configs/Internal/Directory.hs
@@ -1,21 +1,24 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Obelisk.Configs.Internal.Directory where
 
+import Control.Monad
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Text (Text)
+import qualified Data.Text as T
+import System.Directory
+import System.FilePath.Posix
 
 getConfigsFromDirectory :: FilePath -> IO (Map Text ByteString)
-getConfigsFromDirectory base = do
-  doesDirectoryExist base >>= \case
-    True -> do
-      ps <- listDirectory base
-      fmap mconcat $ forM ps $ \p -> do
-        subdirConfigs <- getConfigsFromDirectory $ base </> p
-        pure $ Map.mapKeys (T.pack . (p </>) . T.unpack) subdirConfigs
-    False -> do
-      doesFileExist base >>= \case
-        True -> do
-          f <- BS.readFile base
-          pure $ Map.singleton "" f
-        False -> do
-          pure mempty
+getConfigsFromDirectory base = doesDirectoryExist base >>= \case
+  True -> do
+    ps <- listDirectory base
+    fmap mconcat $ forM ps $ \p -> do
+      subdirConfigs <- getConfigsFromDirectory $ base </> p
+      pure $ Map.mapKeys (T.pack . (p </>) . T.unpack) subdirConfigs
+  False -> doesFileExist base >>= \case
+    True -> Map.singleton "" <$> BS.readFile base
+    False -> pure mempty
