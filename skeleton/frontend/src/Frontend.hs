27a66a0daed69ad785627bd3fcd2db8efa7e1b94
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-24 09:38:04 -0700

    Introduce CSS file to skeleton project

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -1,24 +1,30 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
+
 module Frontend where
 
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+
 import Obelisk.Frontend
 import Obelisk.Configs
 import Obelisk.Route
+import Obelisk.Generated.Static
+
 import Reflex.Dom.Core
 
 import Common.Api
 import Common.Route
-import Obelisk.Generated.Static
+
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
-  { _frontend_head = el "title" $ text "Obelisk Minimal Example"
+  { _frontend_head = do
+      el "title" $ text "Obelisk Minimal Example"
+      elAttr "link" ("href" =: (static @ "main.css") <> "type" =: "text/css" <> "rel" =: "stylesheet") blank
   , _frontend_body = do
-      text "Welcome to Obelisk!"
+      el "h1" $ text "Welcome to Obelisk!"
       el "p" $ text $ T.pack commonStuff
       elAttr "img" ("src" =: static @"obelisk.jpg") blank
       el "div" $ do
