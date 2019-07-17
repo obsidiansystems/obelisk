5defb8cfc4f61c50c4078e7b0a21cd338f4db6eb
Elliot Cameron <eacameron@gmail.com>
2019-07-17 11:25:19 -0400

    Allow users to configure extra ghci arguments via default.nix

--- skeleton/default.nix
+++ skeleton/default.nix
@@ -13,4 +13,6 @@ project ./. ({ ... }: {
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
-})
+}) // {
+  dev.extraGhciArgs = [];
+}
