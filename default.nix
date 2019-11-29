effb502a6a15a916301ada984aa01950d68cc105
Matthew Bauer <mjbauer95@gmail.com>
2019-11-29 18:10:09 -0500

    Use staticWhich for ghcid, and add custom source
    This adds a custom ghcid pointing to
    https://github.com/ndmitchell/ghcid/pull/288 for better error
    messages. Also uses staticWhich to find ghcid for use in ob run. As a
    result, we no include ghcid in Obelisk runtime.
    
    Fixes #563

--- default.nix
+++ default.nix
@@ -39,6 +39,8 @@ let
           doCheck = false;
         });
         hnix-store-core = self.callHackage "hnix-store-core" "0.1.0.0" {};
+
+        ghcid = haskellLib.justStaticExecutables (self.callCabal2nix "ghcid" (hackGet ./dep/ghcid) {});
       })
 
       pkgs.obeliskExecutableConfig.haskellOverlay
@@ -53,7 +55,7 @@ let
         obelisk-asset-serve-snap = self.callCabal2nix "obelisk-asset-serve-snap" (cleanSource ./lib/asset/serve-snap) {};
         obelisk-backend = self.callCabal2nix "obelisk-backend" (cleanSource ./lib/backend) {};
         obelisk-cliapp = self.callCabal2nix "obelisk-cliapp" (cleanSource ./lib/cliapp) {};
-        obelisk-command = haskellLib.overrideCabal (self.callCabal2nix "obelisk-command" (cleanSource ./lib/command) {}) { librarySystemDepends = [ pkgs.nix ]; };
+        obelisk-command = haskellLib.overrideCabal (self.callCabal2nix "obelisk-command" (cleanSource ./lib/command) {}) { librarySystemDepends = [ pkgs.nix self.ghcid ]; };
         obelisk-frontend = self.callCabal2nix "obelisk-frontend" (cleanSource ./lib/frontend) {};
         obelisk-run = self.callCabal2nix "obelisk-run" (cleanSource ./lib/run) {};
         obelisk-route = self.callCabal2nix "obelisk-route" (cleanSource ./lib/route) {};
