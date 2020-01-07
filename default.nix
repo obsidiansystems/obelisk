47967d83837d0ca82ccffb80aed7ec5dd8808650
Ariel Nunez <ingenieroariel@gmail.com>
2020-01-06 19:53:55 -0500

    Added wrapper to run and run_ that adds verbose flag

--- default.nix
+++ default.nix
@@ -121,12 +121,12 @@ in rec {
   shell = pinBuildInputs "obelisk-shell" ([command] ++ commandRuntimeDeps pkgs);
 
   selftest = pkgs.writeScript "selftest" ''
-    #!/usr/bin/env bash
+    #!${pkgs.runtimeShell}
     set -euo pipefail
 
     PATH="${command}/bin:$PATH"
     export OBELISK_IMPL="$(mktemp -d)"
-    git clone file://${pathGit} $OBELISK_IMPL
+    ${pkgs.git}/bin/git clone file://${pathGit} $OBELISK_IMPL
     "${ghcObelisk.obelisk-selftest}/bin/obelisk-selftest" +RTS -N -RTS "$@"
   '';
   skeleton = pkgs.runCommand "skeleton" {
