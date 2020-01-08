5af2b3b47b82261d92c5aa17645681b0f8147d52
Ariel Nunez <ingenieroariel@gmail.com>
2020-01-08 18:55:13 -0500

    Bring back release.nix behavior

--- release.nix
+++ release.nix
@@ -2,9 +2,7 @@
   supportedPlatforms ? [ "x86_64-linux" "x86_64-darwin" ]
 }:
 let 
-  obelisk-everywhere = (import ./everywhere.nix { cacheBuildSystems = supportedPlatforms; }).metaCache;
-  obelisk-test = import ./test.nix;
-in {
-  build = obelisk-everywhere;
-  test = obelisk-test;
+  obelisk-everywhere = import ./everywhere.nix { cacheBuildSystems = supportedPlatforms; };
+in obelisk-everywhere // {
+  test = import ./test.nix;
 }
