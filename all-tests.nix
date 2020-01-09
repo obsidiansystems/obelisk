55b3fa7c78f5bfbba789194d37d77450ba849d9b
Elliot Cameron <eacameron@gmail.com>
2020-01-08 21:10:37 -0500

    Remove trailing whitespace in all-tests.nix

--- all-tests.nix
+++ all-tests.nix
@@ -1,7 +1,7 @@
 {
   supportedSystems ? [ builtins.currentSystem ]
 }:
-let 
+let
   nginxRoot = "/run/nginx";
   obelisk = import ./default.nix {};
   # Get NixOS a pre-release 20.03 that contains the python based tests and recursive nix
@@ -21,7 +21,7 @@ in
           enable = true;
         };
         environment.systemPackages = [
-          pkgs.git 
+          pkgs.git
         ];
         users.users.root.openssh.authorizedKeys.keys = [
           snakeOilPublicKey
@@ -38,7 +38,7 @@ in
           obelisk.command
           obelisk.shell
           obelisk-everywhere
-          pkgs.git 
+          pkgs.git
         ];
       };
     };
@@ -58,10 +58,10 @@ in
       in ''
       start_all()
       githost.wait_for_open_port("22")
-      
+
       with subtest("test obelisk is installed"):
           client.succeed("ob --help")
-      
+
       with subtest("test the client can access the server via ssh"):
           client.succeed("mkdir -p ~/.ssh/")
           client.succeed(
@@ -75,11 +75,11 @@ in
               "cp ${sshConfigFile} ~/.ssh/config"
           )
           client.wait_until_succeeds("ssh githost true")
-      
+
       with subtest("test a remote bare repo can be started"):
           githost.succeed("mkdir -p ~/myorg/myapp.git")
           githost.succeed("cd ~/myorg/myapp.git && git init --bare")
-      
+
       with subtest("test a git project can be configured with a remote using ssh"):
           client.succeed("mkdir -p ~/code/myapp")
           client.succeed("cd ~/code/myapp && git init")
@@ -91,16 +91,16 @@ in
           client.succeed(
               "cd ~/code/myapp && git remote add origin root@githost:/root/myorg/myapp.git"
           )
-      
+
       with subtest("test pushing code to the remote"):
           client.succeed("cd ~/code/myapp && git push -u origin master")
           client.succeed("cd ~/code/myapp && git status")
-      
+
       with subtest("test obelisk can pack"):
           client.succeed("ob thunk pack ~/code/myapp")
           client.succeed("grep -qF 'git' ~/code/myapp/default.nix")
           client.succeed("grep -qF 'myorg' ~/code/myapp/git.json")
-      
+
       with subtest("test obelisk can detect private repos"):
           client.succeed("""grep -qF '"private": false' ~/code/myapp/git.json""")
     '';
