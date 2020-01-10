1483b2b0068f76e612afaa6187d26188e9704750
Elliot Cameron <eacameron@gmail.com>
2020-01-10 17:34:31 -0500

    Add metaCache back to release.nix

--- release.nix
+++ release.nix
@@ -1,8 +1,10 @@
 {
   supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
 }:
-{ 
+rec {
   recurseForDerivations = true;
   build = import ./all-builds.nix { inherit supportedSystems; };
   test = import ./all-tests.nix { inherit supportedSystems; };
+
+  inherit (build) metaCache;
 }
