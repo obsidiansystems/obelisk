64173d330046d4e19c0f22eeb2e15b322e0e6dd7
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-04-11 21:28:59 +0100

    Portaggedon

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Frontend where
@@ -8,7 +10,7 @@ import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Language.Javascript.JSaddle (eval, liftJSM)
-
+import Prelude hiding (log)
 import Obelisk.Frontend
 import Obelisk.Configs
 import Obelisk.Route
@@ -31,7 +33,7 @@ frontend = Frontend
   , _frontend_body = do
       el "h1" $ text "Welcome to Obelisk!"
       el "p" $ text $ T.pack commonStuff
-      
+
       -- `prerender` and `prerender_` let you choose a widget to run on the server
       -- during prerendering and a different widget to run on the client with
       -- JavaScript. The following will generate a `blank` widget on the server and
@@ -44,5 +46,31 @@ frontend = Frontend
         case exampleConfig of
           Nothing -> text "No config file found in config/common/example"
           Just s -> text $ T.decodeUtf8 s
+
+      let url = "http://localhost:65536"
+          req = xhrRequest "GET" url def
+          cb = log . \case
+            Left e -> "Left: " <> tshow e
+            Right r -> ("Right: " <>) $ T.intercalate " / "
+              [ "status: " <> tshow (_xhrResponse_status r)
+              , "response: " <> tshow (_xhrResponse_responseText r)
+              , "headers: " <> tshow (_xhrResponse_headers r)
+              ]
+          log txt = liftJSM $ void $ eval ("console.log('" <> txt <> "')" :: T.Text)
+          tshow = T.pack . show
+
+      prerender_ blank $ do
+        log "0"
+        void $ newXMLHttpRequestWithError req cb
+        log "1"
+        pb <- getPostBuild
+        log "2"
+        res <- getAndDecode (url <$ pb)
+        log "3"
+        void $ runWithReplace blank $ ffor res $ log . \case
+          Nothing -> "Got nothing"
+          Just () -> "Got something"
+        log "4"
+
       return ()
   }
