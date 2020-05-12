7fa81bf7fc0f4a447f25ecc607655e87b7903088
Tom Smalley <tfsmalley@gmail.com>
2020-05-12 19:19:06 +0100

    Move CPP pragma to the top

--- lib/route/src/Obelisk/Route.hs
+++ lib/route/src/Obelisk/Route.hs
@@ -1,6 +1,6 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE EmptyCase #-}
 {-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE FlexibleContexts #-}
