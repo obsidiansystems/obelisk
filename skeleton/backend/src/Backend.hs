74d784355b80763f188a22c377e4194f942478f5
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-06-17 23:44:40 +0100

    de-reproduce

--- skeleton/backend/src/Backend.hs
+++ skeleton/backend/src/Backend.hs
@@ -4,11 +4,8 @@
 
 module Backend where
 
-import Control.Monad
-import Obelisk.App
-import Obelisk.CliApp
-import Obelisk.Command
 import Obelisk.Backend
+import System.Process
 import System.Which
 
 import Common.Route
@@ -16,10 +13,8 @@ import Common.Route
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
   { _backend_run = \_serve -> do
-      cfg <- mkObeliskConfig
-      print <=< runObelisk cfg
-        $ readProcessAndLogOutput (Debug, Debug)
-        $ proc $(staticWhich "git") ["clone", "git@github.com:obsidiansystems/obelisk.git"]
+      _ <- spawnProcess $(staticWhich "git") ["clone", "git@github.com:obsidiansystems/obelisk.git"]
+      pure ()
 
   , _backend_routeEncoder = fullRouteEncoder
   }
