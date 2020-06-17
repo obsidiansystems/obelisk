acd408ae65c4457c3c7317abfcea571acf69b4f7
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-06-17 23:23:05 +0100

    reproduce

--- skeleton/backend/src/Backend.hs
+++ skeleton/backend/src/Backend.hs
@@ -1,10 +1,25 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Backend where
 
-import Common.Route
+import Control.Monad
+import Obelisk.App
+import Obelisk.CliApp
+import Obelisk.Command
 import Obelisk.Backend
+import System.Which
+
+import Common.Route
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \_serve -> do
+      cfg <- mkObeliskConfig
+      print <=< runObelisk cfg
+        $ readProcessAndLogOutput (Debug, Debug)
+        $ proc $(staticWhich "git") ["clone", "git@github.com:obsidiansystems/obelisk.git"]
+
   , _backend_routeEncoder = fullRouteEncoder
   }
--- skeleton/default.nix
+++ skeleton/default.nix
@@ -8,9 +8,18 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
+project ./. ({ pkgs, hackGet, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+  overrides = self: super: with pkgs.haskell.lib; {
+    inherit (import ../haskell-overlays/obelisk.nix self super) obelisk-command;
+    backend = overrideCabal (super.backend) (drv: {
+      librarySystemDepends = (drv.librarySystemDepends or []) ++ [
+        pkgs.git
+      ];
+    });
+  };
 })
