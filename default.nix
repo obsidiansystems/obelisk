5c656cb36843c048184ad99344ef9537daaf1d02
Elliot Cameron <3noch@users.noreply.github.com>
2020-07-21 15:22:48 -0400

    Use stdout for nginx's access log by default
    Otherwise nginx will save access logs to disk which can fill up the disk

--- default.nix
+++ default.nix
@@ -137,6 +137,9 @@ in rec {
           locations.${baseUrl} = {
             proxyPass = "http://127.0.0.1:" + toString internalPort;
             proxyWebsockets = true;
+            extraConfig = ''
+              access_log stdout;
+            '';
           };
         };
       };
