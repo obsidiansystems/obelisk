d32a60fea5293d3d10876fc883863aba0eb62f0c
Tom Smalley <tfsmalley@gmail.com>
2020-09-22 21:25:59 +0100

    Fix skeleton

--- skeleton/backend/src/Backend.hs
+++ skeleton/backend/src/Backend.hs
@@ -3,7 +3,7 @@ module Backend where
 import Common.Route
 import Obelisk.Backend
 
-backend :: Backend MyDomain BackendRoute FrontendRoute
+backend :: Backend () BackendRoute FrontendRoute
 backend = Backend
   { _backend_run = \serve -> serve $ const $ return ()
   , _backend_routeEncoder = fullRouteEncoder
