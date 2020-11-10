57c4153d32bb4ac6adc9e93cd0991e89ec894b80
Ali Abrar <aliabrar@gmail.com>
2020-11-09 20:45:43 -0500

    route: Add missing dep

--- lib/route/src/Obelisk/Route.hs
+++ lib/route/src/Obelisk/Route.hs
@@ -170,6 +170,7 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Data.Text.Lens (IsText, packed, unpacked)
+import Data.Type.Equality
 import Data.Universe
 import Data.Universe.Some
 import Network.HTTP.Types.URI
