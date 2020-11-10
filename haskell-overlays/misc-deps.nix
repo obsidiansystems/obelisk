bb40aa3c68f79c5d12252ee25ddcf588d882eec4
Ali Abrar <aliabrar@gmail.com>
2020-11-09 20:49:55 -0500

    haskell-overlays: aeson-gadt-th -> 0.2.4

--- haskell-overlays/misc-deps.nix
+++ haskell-overlays/misc-deps.nix
@@ -16,6 +16,8 @@ in
   hnix = haskellLib.dontCheck (self.callHackage "hnix" "0.8.0" {});
   hnix-store-core = self.callHackage "hnix-store-core" "0.2.0.0" {};
 
+  aeson-gadt-th = self.callHackage "aeson-gadt-th" "0.2.4" {};
+
   ghcid = self.callCabal2nix "ghcid" (hackGet ../dep/ghcid) {};
   # Exports more internals
   snap-core = haskellLib.dontCheck (self.callCabal2nix "snap-core" (hackGet ../dep/snap-core) {});
