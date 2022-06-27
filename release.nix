4ed1275a66c32cb5bcef45b7424c3a65b1826749
cidkidnix <cidkidnix@protonmail.com>
2022-06-27 13:14:45 -0500

    update

--- release.nix
+++ release.nix
@@ -1,9 +1,8 @@
-{
-  supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
+{ supportedSystems ? [ "x86_64-linux" ]
 }:
 rec {
   recurseForDerivations = true;
-  build = import ./all-builds.nix { inherit supportedSystems; version = "ghc810"; };
+  build = import ./all-builds.nix { inherit supportedSystems; version = "ghc-8.10.7"; };
   test = import ./all-tests.nix { inherit supportedSystems; };
 
   inherit (build) metaCache;
