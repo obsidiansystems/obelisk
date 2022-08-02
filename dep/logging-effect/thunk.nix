68b01ff6e1d8159be2adec7b04d93d1087f3b0a8
Amélia Liao <me@amelia.how>
2022-08-02 11:25:34 -0300

    fix nix-thunk thunk

--- dep/logging-effect/thunk.nix
+++ dep/logging-effect/thunk.nix
@@ -2,7 +2,7 @@
 let fetch = { private ? false, fetchSubmodules ? false, owner, repo, rev, sha256, ... }:
   if !fetchSubmodules && !private then builtins.fetchTarball {
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz"; inherit sha256;
-  } else (import <nixpkgs> {}).fetchFromGitHub {
+  } else (import "/nix/store/qjg458n31xk1l6lj26c3b871d4i4is98-source" {}).fetchFromGitHub {
     inherit owner repo rev sha256 fetchSubmodules private;
   };
   json = builtins.fromJSON (builtins.readFile ./github.json);
