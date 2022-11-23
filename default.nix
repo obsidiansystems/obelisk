0836d02c251ea522b255a92fe4543ca121e5b22a
hsloan <ishaq.sloan@obsidian.systems>
2022-11-23 15:53:14 +0000

    ghc810 true

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
     security.acme.acceptTerms = false;
   }
 , reflex-platform-func ? import ./dep/reflex-platform
-, useGHC810 ? false #true if one wants to use ghc 8.10.7
+, useGHC810 ? true #true if one wants to use ghc 8.10.7
 }:
 let
   reflex-platform = getReflexPlatform { inherit system; };
