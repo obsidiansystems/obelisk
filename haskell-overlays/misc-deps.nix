2e3b47dc0ce5ca565ff58b986a09eeca6f5a6c19
Luigy Leon <luigy@outlook.com>
2023-02-06 18:39:16 -0500

    Fix ob run for ghc 8.{6,10}

--- haskell-overlays/misc-deps.nix
+++ haskell-overlays/misc-deps.nix
@@ -10,11 +10,10 @@ let
 in
 
 rec {
-  resolv = haskellLib.dontCheck (self.callHackage "resolv" "0.1.2.0" {});
-  cabal-install = haskellLib.doJailbreak ((self.callHackage "cabal-install" "3.4.1.0" {}).overrideScope (self: super: { Cabal = self.Cabal_3_4_0_0; }));
-
   # hpack requires cabal >= 3.0 but the ghc865 package set builds it with 2.4 by default
-  hpack = super.hpack.overrideScope (self: super: { Cabal = self.Cabal_3_2_1_0; });
+  hpack = if __useNewerCompiler
+    then super.hpack
+    else super.hpack.overrideScope (self: super: { Cabal = self.Cabal_3_2_1_0; });
 
   # These versions work with both the ghc865 and ghc8107 package sets
   git = self.callCabal2nix "git" (hackGet ../dep/hs-git) { };
@@ -23,12 +22,7 @@ rec {
   universe-dependent-sum-810 = self.callHackage "universe-dependent-sum" "1.3" {};
   universe-some-810 = haskellLib.dontHaddock (haskellLib.appendBuildFlags (haskellLib.doJailbreak (self.callHackage "universe-some" "1.2" { })) [ "--ghc-option=-Wno-inferred-safe-imports" "--ghc-option=-Wno-missing-safe-haskell-mode" ]);
 
-  stylish-haskell = null; # FIXME
-  beam-migrate = self.callHackageDirect {
-    pkg = "beam-migrate";
-    ver = "0.5.1.2";
-    sha256 = "sha256-vEv/6DCvuEq6cmxoPKxZNIm5g6YUgrdvAK4YAoZQr/E=";
-  } {};
+  beam-migrate = self.callHackage "beam-migrate" "0.5.1.2" {};
 
   universe-810 = self.callHackage "universe" "1.2" {};
   universe-instances-extended-810 = self.callHackage "universe-instances-extended" "1.1.1" {};
@@ -39,7 +33,6 @@ rec {
 
   universe-86 = haskellLib.dontCheck (self.callHackage "universe" "1.2" {});
   universe-instances-extended-86 = self.callHackage "universe-instances-extended" "1.1.1" {};
-  hnix-86 = haskellLib.dontCheck super.hnix;
 
   universe = mkVersionset __useNewerCompiler universe-86 universe-810;
   universe-instances-extended = mkVersionset __useNewerCompiler universe-instances-extended-86 universe-instances-extended-810;
@@ -49,38 +42,36 @@ rec {
   universe-some-86 = self.callHackage "universe-some" "1.2" {};
   universe-some = mkVersionset __useNewerCompiler universe-some-86 universe-some-810;
 
-  regex-base = self.callHackage "regex-base" "0.94.0.0" { };
-  regex-posix = self.callHackage "regex-posix" "0.96.0.0" { };
-  regex-tdfa = self.callHackage "regex-tdfa" "1.3.1.0" { };
-  test-framework = haskellLib.dontCheck (self.callHackage "test-framework" "0.8.2.0" { });
-
   hnix-store-core = haskellLib.dontCheck super.hnix-store-core;
   hnix-store = haskellLib.dontCheck super.hnix-store;
 
-  # https://github.com/haskell/hackage-security/issues/247
-  hackage-security = haskellLib.dontCheck super.hackage-security; # only tests use aeson and are not compat with 1.5;
-  heist = haskellLib.dontCheck (haskellLib.doJailbreak super.heist); # aeson 1.5 bump
-  aeson-gadt-th = haskellLib.doJailbreak super.aeson-gadt-th; # requires aeson 1.5 for ghc8.10 support?
-  deriving-compat = self.callHackage "deriving-compat" "0.6" { };
-  http-api-data = haskellLib.doJailbreak super.http-api-data;
   nix-derivation = haskellLib.doJailbreak super.nix-derivation;
-  algebraic-graphs = haskellLib.doJailbreak super.algebraic-graphs;
-  snap = haskellLib.doJailbreak super.snap;
   ghcid = self.callCabal2nix "ghcid" (hackGet ../dep/ghcid) { };
-  # Exports more internals
-  snap-core = haskellLib.dontCheck (self.callCabal2nix "snap-core" (hackGet ../dep/snap-core) { });
 
-  logging-effect = self.callCabal2nix "logging-effect" (hackGet ../dep/logging-effect) { };
-  resourcet = self.callHackage "resourcet" "1.2.4.2" { };
-  unliftio-core = self.callHackage "unliftio-core" "0.2.0.1" { };
   shelly = self.callHackage "shelly" "1.9.0" { };
   # version >= 0.2.5.2 has a Cabal version of 3.0, which nix doesn't like
   vector-binary-instances = self.callHackage "vector-binary-instances" "0.2.5.1" {};
-  modern-uri = haskellLib.doJailbreak super.modern-uri;
-  monad-logger = self.callHackage "monad-logger" "0.3.36" { };
+  modern-uri = if __useNewerCompiler
+    then super.modern-uri
+    else haskellLib.doJailbreak super.modern-uri;
   neat-interpolation = haskellLib.doJailbreak super.neat-interpolation;
-  nix-thunk = (import ../dep/nix-thunk { }).makeRunnableNixThunk (self.callCabal2nix "nix-thunk" (hackGet ../dep/nix-thunk) { });
-  cli-extras = self.callCabal2nix "cli-extras" (hackGet ../dep/cli-extras) { };
+  nix-thunk = (import ../dep/nix-thunk { }).makeRunnableNixThunk (haskellLib.doJailbreak (self.callCabal2nix "nix-thunk" (hackGet ../dep/nix-thunk) { }));
+
+  # aeson 2.0 & attoparsec 0.14 support overrides
+  binary-instances = haskellLib.doJailbreak (self.callHackage "binary-instances" "1.0.3" {});
+  binary-orphans = self.callHackage "binary-orphans" "1.0.3" {};
+  semialign-indexed = haskellLib.doJailbreak (self.callHackage "semialign-indexed" "1.2" {});
+  io-streams = self.callHackage "io-streams" "1.5.2.1" {};
+  io-streams-haproxy = self.callHackage "io-streams-haproxy" "1.0.1.0" {};
+  snap-core = self.callHackage "snap-core" "1.0.5.0" {};
+  snap-server = haskellLib.doJailbreak super.snap-server;
+  cborg = self.callHackage "cborg" "0.2.8.0" {};
+  heist = haskellLib.dontCheck (self.callHackage "heist" "1.1.1.0" {});
+  github = self.callHackage "github" "0.28.0.1" {};
+  http-streams = self.callHackage "http-streams" "0.8.9.6" {};
+  http-common = self.callHackage "http-common" "0.8.3.4" {};
+
+  cli-extras = haskellLib.doJailbreak (self.callCabal2nix "cli-extras" (hackGet ../dep/cli-extras) { });
   cli-git = haskellLib.overrideCabal (self.callCabal2nix "cli-git" (hackGet ../dep/cli-git) { }) {
     librarySystemDepends = with pkgs; [
       gitMinimal
