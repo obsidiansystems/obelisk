352abe62d0cd6234dcf355b7216a76be66001481
Ali Abrar <ali.abrar@obsidian.systems>
2023-09-08 21:44:01 -0400

    ci: add ghc on aarch64-darwin to cache

--- release.nix
+++ release.nix
@@ -4,6 +4,7 @@ rec {
   recurseForDerivations = true;
   build = import ./all-builds.nix { inherit supportedSystems; };
   test = import ./all-tests.nix { inherit supportedSystems; };
+  beta = (import ./skeleton { system = "aarch64-darwin"; }).shells.ghc;
 
   inherit (build) metaCache;
 }
