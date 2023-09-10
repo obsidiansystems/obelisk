a3a3cc4b8c3f0b701860b24ab53f4fa420f9c8d6
Ali Abrar <ali.abrar@obsidian.systems>
2023-09-10 07:19:07 -0400

    ci: cache aarch64-darwin command

--- release.nix
+++ release.nix
@@ -4,7 +4,10 @@ rec {
   recurseForDerivations = true;
   build = import ./all-builds.nix { inherit supportedSystems; };
   test = import ./all-tests.nix { inherit supportedSystems; };
-  beta = (import ./skeleton { system = "aarch64-darwin"; }).shells.ghc;
+  beta = {
+    skeleton =(import ./skeleton { system = "aarch64-darwin"; }).shells.ghc;
+    command =(import ./.{ system = "aarch64-darwin"; }).command;
+  };
 
   inherit (build) metaCache;
 }
