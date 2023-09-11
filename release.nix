bc74d835e6aecda883c63eb3dabe1c8190b089ba
Ali Abrar <ali.abrar@obsidian.systems>
2023-09-10 19:38:11 -0700

    ci: build skeleton exe on aarch64-darwin

--- release.nix
+++ release.nix
@@ -5,8 +5,9 @@ rec {
   build = import ./all-builds.nix { inherit supportedSystems; };
   test = import ./all-tests.nix { inherit supportedSystems; };
   beta = {
-    skeleton =(import ./skeleton { system = "aarch64-darwin"; }).shells.ghc;
-    command =(import ./.{ system = "aarch64-darwin"; }).command;
+    skeleton = (import ./skeleton { system = "aarch64-darwin"; }).shells.ghc;
+    skeletonExe = (import ./skeleton { system = "aarch64-darwin"; }).exe;
+    command = (import ./.{ system = "aarch64-darwin"; }).command;
   };
 
   inherit (build) metaCache;
