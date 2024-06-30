7a3ccf6642f2c99fc89844a196e1df92da289266
Alexandre Esteves <alexfmpe@proton.me>
2024-06-30 06:01:11 +0100

    Add HasInitialObject Encoder instance

--- lib/route/src/Obelisk/Route.hs
+++ lib/route/src/Obelisk/Route.hs
@@ -136,6 +136,7 @@ import Control.Applicative
 import Control.Category (Category (..))
 import qualified Control.Categorical.Functor as Cat
 import Control.Categorical.Bifunctor
+import Control.Categorical.Object
 import Control.Category.Associative
 import Control.Category.Monoidal
 import Control.Category.Braided
@@ -206,6 +207,7 @@ import Data.Text.Lens (IsText, packed, unpacked)
 import Data.Type.Equality
 import Data.Universe
 import Data.Universe.Some
+import Data.Void
 import Network.HTTP.Types.URI
 import qualified Numeric.Lens
 import Obelisk.Route.TH
@@ -392,6 +394,17 @@ instance (Applicative check, Monad parse) => Semigroupoid (Encoder check parse) 
 instance (Applicative check, Monad parse) => Category (Encoder check parse) where
   id = Encoder $ pure id
   (.) = o
+
+instance MonadError Text parse => HasInitialObject (EncoderImpl parse) where
+  type Initial (EncoderImpl parse) = Void
+  initiate = EncoderImpl
+    { _encoderImpl_encode = \case
+    , _encoderImpl_decode = \_ -> throwError "initiate: can't decode anything"
+    }
+
+instance (Applicative check, MonadError Text parse) => HasInitialObject (Encoder check parse) where
+  type Initial (Encoder check parse) = Initial (EncoderImpl parse)
+  initiate = Encoder $ pure $ initiate
 
 instance Monad parse => Category (EncoderImpl parse) where
   id = EncoderImpl
@@ -1120,10 +1133,7 @@ instance UniverseSome Void1 where
 instance FiniteSome Void1
 
 void1Encoder :: (Applicative check, MonadError Text parse) => Encoder check parse (Some Void1) a
-void1Encoder = Encoder $ pure $ EncoderImpl
-  { _encoderImpl_encode = foldSome $ \case
-  , _encoderImpl_decode = \_ -> throwError "void1Encoder: can't decode anything"
-  }
+void1Encoder = initiate . viewEncoder (iso (foldSome (\case)) (\case))
 
 instance GShow Void1 where
   gshowsPrec _ = \case {}
