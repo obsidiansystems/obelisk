a9ed1b07dac2f123d1969c75bde5d812b6ebc743
Ryan Trinkle <ryan@trinkle.org>
2024-09-30 15:58:35 -0400

    Improve shell to support interpreting deps

--- default.nix
+++ default.nix
@@ -477,20 +477,16 @@ in rec {
         };
 
         #combinedShell = self.shells.ghc;
-        combinedShell = self.shellFor {
-          withHoogle = false;
-          tools = {
-            cabal = "3.2.0.0";
+        combinedShell =
+          let packagesToInclude = ["backend" "common" "frontend"];
+          in self.shellFor {
+            withHoogle = false;
+            tools = {
+              cabal = "3.2.0.0";
+            };
+            packages = ps: builtins.map (name: ps.${name}) packagesToInclude;
+            additional = ps: builtins.attrValues (builtins.removeAttrs ps packagesToInclude);
           };
-          packages = ps: with ps; [
-            backend
-            common
-          ];
-          additional = ps: with ps; [
-            obelisk-backend
-            obelisk-run
-          ];
-        };
 
         hoogleShell = self.shellFor {
           withHoogle = true;
