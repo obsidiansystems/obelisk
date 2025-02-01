01308c0d200b071e0e9b7d2d652a2023b6805049
Alexandre Esteves <alexfmpe@proton.me>
2025-02-01 13:13:58 +0000

    Fix unused import in older GHC

--- lib/backend/src/Obelisk/Backend.hs
+++ lib/backend/src/Obelisk/Backend.hs
@@ -48,14 +48,12 @@ import Data.Monoid ((<>))
 #endif
 #if __GLASGOW_HASKELL__ >= 906
 import Control.Monad
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class (MonadIO (liftIO))
 #else
 import Control.Monad.Except
 #endif
 #endif
 
-import Control.Monad
-import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BSC8
 import Data.Default (Default (..))
