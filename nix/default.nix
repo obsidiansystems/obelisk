45b5745ef6239b0b89a401d75e676e925a683eb9
ymeister <47071325+ymeister@users.noreply.github.com>
2026-03-22 19:13:20 +0900

    Add exe.{wasm,js} to project for building backend per frontend target
    Extend the project return value with exe.wasm and exe.js, each building
    the backend executable with the corresponding obelisk.frontend.target
    override. Uses proj.override to reuse the base project evaluation.

--- nix/default.nix
+++ nix/default.nix
@@ -10,10 +10,17 @@ in lib // {
   inherit module;
   inherit (lib) extraCabalProject;
 
-  project = userModule: (nix-haskell {
-    imports = [
-      module
-      userModule
-    ];
-  }).haskell-nix.project;
+  project = userModule:
+    let proj = (nix-haskell {
+          imports = [
+            module
+            userModule
+          ];
+        }).haskell-nix.project;
+    in proj // {
+      exe = {
+        wasm = (proj.override { obelisk.frontend.target = "wasm"; }).hsPkgs.backend.components.exes.backend;
+        js = (proj.override { obelisk.frontend.target = "js"; }).hsPkgs.backend.components.exes.backend;
+      };
+    };
 }
