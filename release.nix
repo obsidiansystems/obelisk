4d25aa0e230c9833e4c8237562002ca7ed5021ed
ymeister <47071325+ymeister@users.noreply.github.com>
2026-03-23 17:21:32 +0900

    Add `skeleton.shell` to `release.nix`

--- release.nix
+++ release.nix
@@ -3,6 +3,7 @@
 let skeleton = import ./skeleton { inherit system; };
 
 in skeleton.nixpkgs.linkFarm "obelisk-release" [
+  { name = "shell"; path = skeleton.shell; }
   { name = "serverExe-wasm"; path = skeleton.serverExe.wasm; }
   { name = "serverExe-js"; path = skeleton.serverExe.js; }
   { name = "containerImage-wasm"; path = skeleton.containerImage.wasm; }
